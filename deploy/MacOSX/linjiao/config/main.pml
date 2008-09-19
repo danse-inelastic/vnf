95ce0ea090aa0921efbc142c4712b5bed88d5846
Jiao Lin <linjiao@caltech.edu>
2008-09-19 23:54:23 +0000

    vnf prototype2:  a way to specify db on a remote machine.
    git-svn-id: svn://danse.us/VNET/vnf/branches/prototype2@31 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- deploy/MacOSX/linjiao/config/main.pml
+++ deploy/MacOSX/linjiao/config/main.pml
@@ -19,6 +19,10 @@
 
   <component name='main'>
     <property name='debug'>1</property>
+<!--
+    <property name='db'>linjiao@localhost:5432:vnf</property>
+-->
+    <property name='db'>vnf:4Od&amp;Cm#@localhost:54320:vnf</property>
     <property name='home'>http://localhost/~linjiao/vnf2/</property>
     <property name='cgi-home'>http://localhost/~linjiao/cgi-bin/vnf2/main.cgi</property>
     <property name='imagepath'>/~linjiao/vnf2/images</property>
--- vnf/applications/WebApplication.py
+++ vnf/applications/WebApplication.py
@@ -149,7 +149,8 @@ class WebApplication(Base):
 
         # connect to the database
         import pyre.db
-        self.db = pyre.db.connect(database=self.inventory.db, wrapper=self.inventory.dbwrapper)
+        dbkwds = DbAddressResolver().resolve(self.inventory.db)
+        self.db = pyre.db.connect(wrapper=self.inventory.dbwrapper, **dbkwds)
 
         # initialize the accessors
         self.clerk.db = self.db
@@ -169,6 +170,59 @@ class WebApplication(Base):
     def _getPrivateDepositoryLocations(self):
         return ['../content', '../config']
 
+
+
+class DbAddressResolver:
+
+    def resolve(self, address):
+        tmp = address.split('@')
+        if len(tmp)==1:
+            svr = tmp[0]
+            up = ''
+        elif len(tmp)==2:
+            up,svr = tmp
+        else:
+            raise ValueError, 'Invalid db address: %r' % address
+
+        host,port,database = self._resolve_svr(svr)
+        user, pw = self._resolve_up(up)
+        ret = {
+            'host': host,
+            'port': port,
+            'database': database,
+            'user': user,
+            }
+        if pw: ret['password'] = pw
+        return ret
+    
+
+    def _resolve_up(self, up):
+        separator = ':'
+        tmp = up.split(separator)
+        if len(tmp) == 1:
+            user = tmp[0]
+            pw = None
+        elif len(tmp) == 2:
+            user, pw = tmp
+        else:
+            raise ValueError, 'Invalid user, password: %r' % up
+        return user, pw
+    
+
+    def _resolve_svr(self, svr):
+        separator = ':'
+        
+        if svr.find(separator) == -1:
+            return 'localhost', 5432, svr
+        splits = svr.split(separator)
+        if len(splits)==2:
+            host, database = splits
+            return host, 5432, database
+        elif len(splits)==3:
+            host, port, database = splits
+            return host, port, database
+        raise ValueError, 'Invalid db svr: %r' % (svr,)
+    
 
 import journal
 journal.debug('curator').activate()
