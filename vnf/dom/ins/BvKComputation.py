02ba1c32f84ed645f5b7b0e3c4f93b0064f0e653
Jiao Lin <linjiao@caltech.edu>
2008-11-13 19:13:02 +0000

    form for bvk dispersion computation
    git-svn-id: svn://danse.us/VNET/vnf/trunk@136 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnf/dom/ins/BvKComputation.py
+++ vnf/dom/ins/BvKComputation.py
@@ -26,9 +26,17 @@ class BvKComputation(base):
                                
     model = pyre.db.reference(name='model', table = BvKModel)
     type = pyre.db.varchar(name='type', length = 16)
+    
     dE = pyre.db.real(name='dE', default = 0.5) # unit meV
-    Qs = pyre.db.versatileReference(name='Qs', tableRegistry = tableRegistry)
+    N1 = pyre.db.integer(name='N1', default = 10) # number of sampling points (in 1 dimension)
 
+
+# types of computation
+types = [
+    ('dos', 'Phonon Density of States'),
+    ('directional', 'Phonon dispersion on a special direction'),
+    ('disp', 'Full phonon dispersion on a grid (can be used in virtual neutron experiment)'),
+    ]
 
 # version
 __id__ = "$Id$"
