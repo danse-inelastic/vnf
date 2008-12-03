ca5c883d5062df3615d919009d2f1479a87b33b2
Jiao Lin <linjiao@caltech.edu>
2008-12-03 22:15:17 +0000

    AbInitio table
    git-svn-id: svn://danse.us/VNET/vnf/trunk@207 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnf/dom/AbInitio.py
+++ vnf/dom/AbInitio.py
@@ -16,31 +16,31 @@ from Computation import Computation as base
 
 class AbInitio(base):
 
-    name = 'abInitio'
+    name = 'abinitio'
 
     import pyre.db
     
-    engine = pyre.db.str('engine', default='vasp')
+    engine = pyre.db.varchar(name='engine', length=64, default='vasp')
 #    engine.validator=pyre.db.choice(['vasp', 'ab init'])
     engine.meta['tip'] = 'Ab initio engine'
     
-    xcFunctional = pyre.db.str('xcFunctional', default='PAW-PBE')
+    xcFunctional = pyre.db.varchar(name='xcFunctional', length=64, default='PAW-PBE')
 #    xcFunctional.validator=pyre.db.choice(['PAW-PBE', 'PAW-GGA', 'LDA'])
     xcFunctional.meta['tip'] = 'Exchange correlation functional'
     
-    kineticEnergyCutoff = pyre.db.float('kineticEnergyCutoff', default=140.0)
+    kineticEnergyCutoff = pyre.db.real(name='kineticEnergyCutoff', default=140.0)
     kineticEnergyCutoff.meta['tip'] = 'Kinetic energy cutoff'
     
-    monkhorstPackMesh = pyre.db.list('monkhorstPackMesh', default=[1,1,1])
+    monkhorstPackMesh = pyre.db.integerArray(name='monkhorstPackMesh', default=[1,1,1])
     monkhorstPackMesh.meta['tip'] = 'Monkhorst pack mesh'
     
-    supercell = pyre.db.list('supercell', default=[1,1,1])
+    supercell = pyre.db.integerArray(name='supercell', default=[1,1,1])
     supercell.meta['tip'] = 'Supercell for phonon calculation'
     
-    displacementAmplitude = pyre.db.float('displacementAmplitude', default=0.01)
+    displacementAmplitude = pyre.db.real(name='displacementAmplitude', default=0.01)
     displacementAmplitude.meta['tip'] = 'Displacement amplitude'      
     
-    qGrid = pyre.db.list('qGrid', default=[1,1,1])
+    qGrid = pyre.db.integerArray(name='qGrid', default=[1,1,1])
     qGrid.meta['tip'] = 'Q grid for phonon calculation'       
 
 
--- vnf/dom/Make.mm
+++ vnf/dom/Make.mm
@@ -35,6 +35,7 @@ all: export
 # export
 
 EXPORT_PYTHON_MODULES = \
+	AbInitio.py \
 	Block.py \
 	Computation.py \
 	ComputationResult.py \
