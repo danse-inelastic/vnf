c58d6acf3bf5ddf81af14fc806258fa255b2e28b
Jiao Lin <linjiao@caltech.edu>
2008-12-08 09:55:02 +0000

    vnf/dom/SingleCrystal.py: added an example record
    git-svn-id: svn://danse.us/VNET/vnf/trunk@225 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnf/dom/SingleCrystal.py
+++ vnf/dom/SingleCrystal.py
@@ -20,6 +20,30 @@ class SingleCrystal(base):
     import pyre.db
 
 
+def inittable(db):
+    def record( id, cartesian_lattice, fractional_coordinates,
+                atom_symbols, chemical_formula, short_description ):
+        r = SingleCrystal()
+        r.id = id
+        r.cartesian_lattice = cartesian_lattice
+        r.fractional_coordinates = fractional_coordinates
+        r.atom_symbols = atom_symbols
+        r.chemical_formula = chemical_formula
+        r.short_description = short_description
+        return r
+
+    records = [
+        record(
+            'silicon_diamond',
+            [2.716,2.716,0, 2.716,0,2.716, 0,2.716,2.716],
+            [0,0,0, 0.25,0.25,0.25],
+            ['Si', 'Si'],
+            'Si',
+            'Si (diamond structure)',
+            ),
+        ]
+    for r in records: db.insertRow( r )
+    return
 
 
 # version
