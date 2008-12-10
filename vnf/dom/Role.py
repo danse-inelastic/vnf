50a73a21da016a53ec3d9bcca55192f79005897f
Jiao Lin <linjiao@caltech.edu>
2008-12-10 00:14:13 +0000

    table: Role
    git-svn-id: svn://danse.us/VNET/vnf/trunk@235 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ vnf/dom/Role.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                             Michael A.G. Aivazis
+#                               Orthologue, Ltd.
+#                      (C) 2004-2006  All Rights Reserved
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+from Table import Table
+
+
+class Role(Table):
+
+
+    import pyre.db
+
+
+    # the table name
+    name = "roles"
+
+    # the table columns
+    id = pyre.db.varchar(name="id", length=64)
+    id.constraints = "PRIMARY KEY"
+
+    description = pyre.db.varchar(name="description", length=255)
+    status = pyre.db.char(name="status", length=1)
+
+
+# version
+__id__ = "$Id: Role.py,v 1.1.1.1 2006-11-27 01:11:25 aivazis Exp $"
+
+# End of file 
