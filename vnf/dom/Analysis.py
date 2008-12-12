9e2a0a911d2774f820764a903d9fc265b92d7e60
J Brandon Keith <jbrkeith@gmail.com>
2008-12-12 22:37:09 +0000

    adding analysis table objects
    git-svn-id: svn://danse.us/VNET/vnf/trunk@248 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ vnf/dom/Analysis.py
@@ -0,0 +1,31 @@
+# -*- Python -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                        (C) 2008  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+from registry import tableRegistry
+
+analysisClasses = ['Sqe','Eisf','Dos','DiffusionCoefficient',
+                       'MeanSquareDisplacement']
+
+from Computation import Computation as base
+class Analysis(base):
+
+    # future base class for all analysis computations
+    pass
+    
+
+
+# version
+__id__ = "$Id$"
+
+# End of file 
