543d306544b0ae002af9b0676fa33a000f78045a
Jiao Lin <linjiao@caltech.edu>
2009-01-22 20:39:33 +0000

    vnf/dom/sans/_all_tables.py: added a table to the registry
    git-svn-id: svn://danse.us/VNET/vnf/trunk@396 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnf/dom/sans/_all_tables.py
+++ vnf/dom/sans/_all_tables.py
@@ -5,6 +5,7 @@ def tables():
 
 
     kernels = [
+        'SANSSphereModelKernel',
         ]
 
 
@@ -24,7 +25,7 @@ def tables():
     
     tables = []
     for t in tablemodules:
-        exec 'from vnf.dom.ins.%s import %s as table' % (t, t) in locals()
+        exec 'from vnf.dom.sans.%s import %s as table' % (t, t) in locals()
         tables.append( table )
         continue
 
