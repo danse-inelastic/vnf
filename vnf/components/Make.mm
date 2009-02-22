a16ab03feb8b19843e91a4bcbf0e692582faa6a8
Jiao Lin <linjiao@caltech.edu>
2009-02-22 16:08:27 +0000

    vnf/dom/Make.mm vnf/components/Make.mm minor (rules for 'tidy', 'clean', ...)
    git-svn-id: svn://danse.us/VNET/vnf/releases/alpha@506 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnf/components/Make.mm
+++ vnf/components/Make.mm
@@ -32,6 +32,16 @@ RECURSE_DIRS = $(BUILD_DIRS) $(OTHER_DIRS)
 
 all: export
 
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+
 #--------------------------------------------------------------------------
 # export
 
