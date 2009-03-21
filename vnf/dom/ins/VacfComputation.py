7f461417a8b653449051538596b630616dfd02f5
J Brandon Keith <jbrkeith@gmail.com>
2009-03-21 07:20:38 +0000

    changing computers
    git-svn-id: svn://danse.us/VNET/vnf/releases/alpha@671 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnf/dom/ins/VacfComputation.py
+++ vnf/dom/ins/VacfComputation.py
@@ -14,7 +14,11 @@
 
 from registry import tableRegistry
 
-from Analysis import Analysis as base
+# eventually generalize this to other types of md
+from vnf.dom.GulpSimulation import GulpSimulation
+
+#from Analysis import Analysis as base
+from vnf.dom.Computation import Computation as base
 
 class VacfComputation(base):
 
@@ -22,12 +26,12 @@ class VacfComputation(base):
 
     import pyre.db
     # the trajectory name can be either a  file name or an id
-    from Trajectory import Trajectory
+    from vnf.dom.Trajectory import Trajectory
     trajectory = pyre.db.reference(name='trajectory', table=Trajectory)
     units = pyre.db.varchar(name='units', length = 16, default = 'nm') # unit meV
     weights = pyre.db.varchar(name='weights', length = 16, default = 'mass') # number of sampling points (in 1 dimension)
 
-
+    gulpsimulation = pyre.db.reference(name='gulpsimulation', table = GulpSimulation)
 
 
 # version
