93452ca4f213b7fcc52e8db688fdcf36883222ef
J Brandon Keith <jbrkeith@gmail.com>
2009-04-30 18:10:23 +0000

    sns service url script
    git-svn-id: svn://danse.us/VNET/vnf/releases/alpha@906 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ tests/snsAuthenticate.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+import base64
+import hmac
+import sha
+import sys
+
+import time
+
+def main():
+
+    #EXPIRES=str(int(time.mktime(time.gmtime())))
+
+    idfile = open("securityToken.id") #security token in a file
+    ACCESS_KEY = idfile.read()
+    idfile.close()
+
+    keyfile = open("snsPrivKey_rsa") #private key (certificates page down at bottom)
+    PRIVATE_KEY = keyfile.read()
+    keyfile.close()
+
+    #if len(sys.argv) < 2:
+    #    print 'Usage: testOrbiterREST.py [URI]'
+    #    sys.exit()
+
+    #[URI] = sys.argv[1:]
+
+    URI =  'https://nucleus.sns.gov/service/user-group-service.php/users'
+
+    ACCESS_KEY='m4ic6c50onp0zt4havhdmati4tdptkb4fj6dmz2xdfn2xiax2f2egmn84qom2ng5'
+
+    EXPIRES='1241110492'
+
+    string = URI + 'OrbiterAccessKeyId=' + ACCESS_KEY + '&Expires=' + EXPIRES
+
+    SIGNATURE = base64.encodestring(hmac.new(PRIVATE_KEY, string, sha).digest()).strip()
+    
+    print 'signature', SIGNATURE
+
+    print 'Test generation of Orbiter VFS SOA URI\n'
+
+    print URI + '?OrbiterAccessKeyId=' + ACCESS_KEY + '&Expires=' + EXPIRES + '&Signature=' + SIGNATURE + '\n'
+
+    from urllib import urlopen
+    print urlopen(URI + '?OrbiterAccessKeyId=' + ACCESS_KEY + '&Expires=' + EXPIRES + '&Signature=' + SIGNATURE).read()
+
+
+if __name__=='__main__':
+	main()
+	
+#17rp6DPaYn640uOmMRdzbFkx890
+
