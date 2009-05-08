72e23770af4c4a3660a3ce737ff6db502a25a810
Jiao Lin <linjiao@caltech.edu>
2009-05-08 17:21:02 +0000

    vnf/alpha: BvK actor: find matter that matches the selected bvk model by hashing
    git-svn-id: svn://danse.us/VNET/vnf/releases/alpha@923 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnf/components/ins/BvK.py
+++ vnf/components/ins/BvK.py
@@ -18,12 +18,31 @@ def getSystem(model, director):
     return open(path).read()
 
 
-def findModels(matter, db):
+def findModels_exact(matter, db):
     from vnf.dom.ins.BvKModel import BvKModel
     b = BvKModel()
     b.matter = matter
     v = b._getFormattedColumnValue('matter')
     return db.fetchall(BvKModel, where="matter='%s'" % v)
+
+
+from vnf.dom.hash import hash
+def findModels(matter, db):
+
+    matter_key = hash(matter, db)
+
+    from vnf.dom.ins.BvKModel import BvKModel
+    candidates = db.fetchall(BvKModel)
+    #ids = [c.id for c in candidates]
+    #matter_keys = [hash(model.matter.dereference(db), db) for model in candidates]
+    #raise "%s, %s" % (matter_key, matter_keys)
+
+    def good(model):
+        matterref = model.matter
+        if not matterref: return False
+        matter = matterref.dereference(db)
+        return hash(matter, db) == matter_key
+    return filter(good, candidates)
 
 
 simpleelementarymatters = [
