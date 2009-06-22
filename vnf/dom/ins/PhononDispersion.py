9fb504c3ec669079fa1a873b0e83e23258b0e857
Jiao Lin <linjiao@caltech.edu>
2009-06-22 19:16:57 +0000

    vnf/dom/ins/PhononDispersion.py: a bit of docs
    git-svn-id: svn://danse.us/VNET/vnf/trunk@1048 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnf/dom/ins/PhononDispersion.py
+++ vnf/dom/ins/PhononDispersion.py
@@ -34,6 +34,17 @@ class PhononDispersion(base1, base2):
     pass # end of Dispersion
 
 
+# Data files
+#   All files must be in IDF format (http://danse.us/trac/inelastic/browser/idf)
+#
+#   * DOS
+#     DOS is optional because it can be created from Omega2
+#   * Omega2, Polarizations
+#     Must be exactly IDF format
+#   * Qgridinfo: 
+#     Must be in IDF format. MAKE SURE THAT THIS Qgridinfo MATCHES THE GRID USED IN Omega2/Polarizations
+
+
 def inittable(db):
     def new(id, creator='vnf'):
         r = PhononDispersion()
