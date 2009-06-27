f8dc12bebc80c4abae508ae35aa01629351c3df0
J Brandon Keith <jbrkeith@gmail.com>
2009-06-27 00:51:44 +0000

    updating jar, new tests, fixed dom for library
    git-svn-id: svn://danse.us/VNET/vnf/releases/alpha@1064 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnf/dom/GulpSimulation.py
+++ vnf/dom/GulpSimulation.py
@@ -12,6 +12,8 @@
 
 
 from MaterialSimulation import MaterialSimulation as base
+from GulpPotential import GulpPotential
+
 class GulpSimulation(base):
 
     name = 'gulpsimulations'
@@ -20,8 +22,9 @@ class GulpSimulation(base):
     
     runtype = pyre.db.varchar(name='runtype', length=64, default='md')
     dos_projections = pyre.db.varcharArray(name='dos_projections', length=32, default=[])
-    
+    potential = pyre.db.reference( name='potential', table = GulpPotential)
 
+    # eventually going to get rid of LIBPOINTER_FILE
     CONFIGURATION_FILE = 'gulp.gin'
     LIBPOINTER_FILE = 'gulp.libs'
     datafiles = [
