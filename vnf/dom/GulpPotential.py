93c38ebed740f69faa360a1fc8365c2a0f59a745
J Brandon Keith <jbrkeith@gmail.com>
2009-07-11 00:48:56 +0000

    changes to db object
    git-svn-id: svn://danse.us/VNET/vnf/releases/alpha@1065 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnf/dom/GulpPotential.py
+++ vnf/dom/GulpPotential.py
@@ -11,20 +11,36 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+from Table import Table
 
-
-from OwnedObject import OwnedObject as base
-class GulpPotential(base):
+class GulpPotential(Table):
 
     name = 'gulppotential'
 
     path = '../content/data/gulppotentials'
 
-    import pyre
-    potential_name = pyre.db.varchar(name='potential_name', length=2048, default='potential')
+    import pyre.db
+    filename = pyre.db.varchar(name='filename', length=2048, default='potential.lib')
     
     elements = pyre.db.varcharArray(name = 'elements', length = 2, default = [] )
     elements.meta['tip'] = 'elements within the potential'
+    
+    description = pyre.db.varchar(name = 'description', length=8192)
+    
+    creator = pyre.db.varchar(name='creator', length=64)
+
+    date = pyre.db.date( name='date' )
+    date.meta['tip'] = 'date of creation'
+    
+    id = pyre.db.varchar(name="id", length=64)
+    id.constraints = 'PRIMARY KEY'
+    id.meta['tip'] = "the unique id"
+
+#    this should be the name of the primary key...for now we *have* to name it 'id'
+#    potential_name = pyre.db.varchar(name="potential_name", length=64)
+#    potential_name.constraints = 'PRIMARY KEY'
+#    potential_name.meta['tip'] = "the unique id"
+
 
 # version
 __id__ = "$Id$"
