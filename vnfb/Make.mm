85fa2144269a6056b566154f70ca469943086f80
Jiao Lin <linjiao@caltech.edu>
2009-08-25 15:52:54 +0000

    vnf: new branch to experiment with luban
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@1133 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ vnfb/Make.mm
@@ -0,0 +1,22 @@
+
+PROJECT = vnfb
+
+# directory structure
+
+RECURSE_DIRS = \
+	bin \
+	cgi-bin \
+	vnfb \
+	log \
+	content \
+	html \
+	config
+
+
+
+#--------------------------------------------------------------------------
+#
+
+all: 
+	BLD_ACTION="all" $(MM) recurse
+
---
+++ vnfb/config/main.pml
@@ -0,0 +1,6 @@
+<inventory >
+  <component name="main">
+    <facility name="actor">vnfb</facility>
+    <property name="log-dir">../log</property>
+  </component>
+</inventory>
