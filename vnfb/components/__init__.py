b3ca18e39ec985e4d8fcb54835cf27b3411eeb2d
Jiao Lin <linjiao@caltech.edu>
2009-08-26 18:11:28 +0000

    vnf-beta-luban: "matter" actor
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@1135 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ vnfb/components/__init__.py
@@ -0,0 +1,19 @@
+# -*- Python -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                      (C) 2006-2009  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+# version
+__id__ = "$Id$"
+
+# Generated automatically by PythonMill on Tue Aug 25 14:40:56 2009
+
+# End of file 
