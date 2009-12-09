08604ee82a7f18370e6adbc5497e5efbf56cf197
Jiao Lin <linjiao@caltech.edu>
2009-12-09 07:33:33 +0000

    vnf-luban:dom:atomicstructure: dom decorators moved from matter package.
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@1392 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/dom/AtomicStructure.py
+++ vnfb/dom/AtomicStructure.py
@@ -13,33 +13,60 @@
 
 
 from _ import o2t
+from dsaw.model.Inventory import Inventory as InvBase
 
 
-from matter.Atom import Atom
-Atom.Inventory.dbtablename = 'atoms'
-AtomTable = o2t(Atom)
-def customizeLubanObjectDrawer(self, drawer):
-    drawer.mold.sequence = ['element', 'xyz', 'label', 'occupancy',]
-Atom.customizeLubanObjectDrawer = customizeLubanObjectDrawer
-
-
-from matter.Lattice import Lattice
-Lattice.Inventory.dbtablename = 'lattices'
-LatticeTable = o2t(Lattice)
-
-def customizeLubanObjectDrawer(self, drawer):
-    drawer.mold.sequence = ['a', 'b', 'c', 'alpha', 'beta', 'gamma']
-Lattice.customizeLubanObjectDrawer = customizeLubanObjectDrawer
-
-
+# data object
+from Atom import Atom
+from Lattice import Lattice
 from matter.Structure import Structure
-Structure.Inventory.dbtablename = 'atomicstructures'
+
+
+# dsaw.model helpers
+def __establishInventory__(self, inventory):
+    inventory.short_description = self.description
+    inventory.lattice = self.lattice
+    inventory.spacegroupno = self.sg.number
+    inventory.chemical_formula = self.getChemicalFormula()
+    inventory.atoms = self # the implementation of Structure class is that structure is inherited from list, and the items are atoms.
+    return
+Structure.__establishInventory__ = __establishInventory__
+
+def __restoreFromInventory__(self, inventory):
+    self.__init__(atoms=inventory.atoms,
+                  lattice=inventory.lattice,
+                  sgid=inventory.spacegroupno,
+                  description=inventory.short_description,
+                  )
+Structure.__restoreFromInventory__ = __restoreFromInventory__
+
+
+# inventory
+from dsaw.model.Inventory import Inventory as InvBase
+class Inventory(InvBase):
+
+    short_description = InvBase.d.str(
+        name = 'short_description', max_length = 256, default ="", label='Description')
+    lattice = InvBase.d.reference(name = 'lattice', targettype=Lattice, owned=1)
+    atoms = InvBase.d.referenceSet(name='atoms', targettype=Atom, owned=1)
+    spacegroupno = InvBase.d.int(name = 'spacegroupno', default =1, label='Spacegroup #')
+    chemical_formula = InvBase.d.str(name='chemical_formula', max_length=1024)
+
+    dbtablename = 'atomicstructures'
+
+Structure.Inventory = Inventory
+
+
+# db table
 StructureTable = o2t(Structure)
 
+
+# view
 def customizeLubanObjectDrawer(self, drawer):
     drawer.sequence = ['lattice', 'atoms', 'properties']
     drawer.mold.sequence = ['short_description', 'spacegroupno']
 Structure.customizeLubanObjectDrawer = customizeLubanObjectDrawer
+
 
 # version
 __id__ = "$Id$"
