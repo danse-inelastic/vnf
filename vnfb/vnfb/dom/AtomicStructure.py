f6b9eea3894d8ab8d7bc41a3f63f52868521caab
Jiao Lin <linjiao@caltech.edu>
2009-12-09 03:33:34 +0000

    vnf-luban: customize luban-generated-view of atomic structure
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@1391 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/dom/AtomicStructure.py
+++ vnfb/vnfb/dom/AtomicStructure.py
@@ -18,17 +18,28 @@ from _ import o2t
 from matter.Atom import Atom
 Atom.Inventory.dbtablename = 'atoms'
 AtomTable = o2t(Atom)
+def customizeLubanObjectDrawer(self, drawer):
+    drawer.mold.sequence = ['element', 'xyz', 'label', 'occupancy',]
+Atom.customizeLubanObjectDrawer = customizeLubanObjectDrawer
 
 
 from matter.Lattice import Lattice
 Lattice.Inventory.dbtablename = 'lattices'
 LatticeTable = o2t(Lattice)
 
+def customizeLubanObjectDrawer(self, drawer):
+    drawer.mold.sequence = ['a', 'b', 'c', 'alpha', 'beta', 'gamma']
+Lattice.customizeLubanObjectDrawer = customizeLubanObjectDrawer
+
 
 from matter.Structure import Structure
 Structure.Inventory.dbtablename = 'atomicstructures'
 StructureTable = o2t(Structure)
 
+def customizeLubanObjectDrawer(self, drawer):
+    drawer.sequence = ['lattice', 'atoms', 'properties']
+    drawer.mold.sequence = ['short_description', 'spacegroupno']
+Structure.customizeLubanObjectDrawer = customizeLubanObjectDrawer
 
 # version
 __id__ = "$Id$"
