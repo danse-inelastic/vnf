6f7e1457e70a3b8371443239b3824c62c058d6ae
Jiao Lin <linjiao@caltech.edu>
2009-12-14 14:58:18 +0000

    vnf-luban: bvk-workflow restarted
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@1444 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ vnfb/vnfb/dom/material_simulations/BvKComputation.py
@@ -0,0 +1,48 @@
+# -*- Python -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                        (C) 2008  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+from dsaw.model.Inventory import Inventory as InvBase
+
+
+from vnfb.dom.AtomicStructure import Structure
+from BvKModel import BvKModel
+
+class BvKComputation(object):
+
+    class Inventory(InvBase):
+        
+        matter = InvBase.d.reference(name='matter', targettype=Structure, owned=0)
+        model = InvBase.d.reference(name='model', targettype = BvKModel, owned=1)
+        
+
+class BvKComputation_GetDOS(BvKComputation):
+
+    class Inventory(InvBase):
+
+        dE = InvBase.d.float(name='dE', default = 0.5) # unit meV
+        N1 = InvBase.d.int(name='N1', default = 10) # number of sampling points (in 1 dimension)
+
+
+# targets of computation
+targets = [
+    ('dos', 'Phonon Density of States'),
+    ('directionaldispersion', 'Phonon dispersion on a special direction'),
+    ('dispersion', 'Full phonon dispersion on a grid (can be used in virtual neutron experiment)'),
+    ]
+
+
+# version
+__id__ = "$Id$"
+
+# End of file 
---
+++ vnfb/vnfb/dom/material_simulations/BvKModel.py
@@ -0,0 +1,61 @@
+# -*- Python -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                        (C) 2008  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+from dsaw.model.Inventory import Inventory as InvBase
+
+
+from vnfb.dom.AtomicStructure import Structure
+from BvKBond import BvKBond
+
+class BvKModel(object):
+
+    class Inventory(InvBase):
+        
+        matter = InvBase.d.reference(name='matter', targettype=Structure, owned=0)
+
+        bonds = InvBase.d.referenceSet(name='bonds', targettype=BvKBond, owned=1)
+
+        dbtablename = 'bvkmodels'
+
+
+# !!! needs update
+def inittable(db):
+    def bvk(id, creater, date, matter, short_description):
+        b = BvKModel()
+        b.id = id
+        b.creater = creater
+        b.date = date
+        b.matter = matter
+        b.short_description = short_description
+        return b
+
+    from PolyCrystal import PolyCrystal
+    bvks = [
+        bvk( 'fccNi-000001', '', '09/26/2008', (PolyCrystal, 'polyxtalfccNi0'),
+             "A bvk model for fcc Ni") 
+        ]
+    for b in bvks: db.insertRow( b )
+    return
+
+
+def initids():
+    return [
+        'fccNi-000001',
+        ]
+
+
+# version
+__id__ = "$Id$"
+
+# End of file 
