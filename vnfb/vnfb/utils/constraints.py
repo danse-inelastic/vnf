20a47999c06623fc933716d217eebe8ca7327c8f
Jiao Lin <linjiao@caltech.edu>
2009-12-15 19:41:47 +0000

    vnf-luban:utils.bvk: added method to find symmetry constraints of force constant tensor
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@1456 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/utils/constraints.py
+++ vnfb/vnfb/utils/constraints.py
@@ -12,12 +12,22 @@
 #
 
 
-class Variable(object):
+# a, b, c, ...
+class Variable:
+
+    def __add__(self, right):
+        return Addition(self, right)
+
+
+    def __mul__(self, right):
+        return Multiplication(self, right)
+
 
     def __eq__(self, right):
         return Equal(self, right)
 
 
+# a = b, a>3, ...
 class Relation(object): pass
 
 
@@ -26,12 +36,55 @@ class BinaryRelation(Relation):
     def __init__(self, left, right):
         self.left = left
         self.right = right
-        
+
+    def __str__(self):
+        return '%s %s %s' % (self.left, self.relation, self.right)
 
 class Equal(BinaryRelation):
 
-    pass
+    relation = '=='
     
+    pass
+
+
+
+# a+b, b*c, ...
+class Operation(object):
+
+    def __eq__(self, right):
+        return Equal(self, right)
+
+
+class BinaryOperation(Operation):
+    
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+    def __str__(self):
+        return '%s %s %s' % (self.left, self.op, self.right)
+
+
+class Addition(BinaryOperation):
+    op = '+'
+    pass
+
+
+class Subtraction(BinaryOperation):
+    op = '-'
+    pass
+
+
+class Multiplication(BinaryOperation):
+    op = '*'
+    pass
+
+
+class Division(BinaryOperation):
+    op = '/'
+    pass
+
 
 # version
 __id__ = "$Id$"
