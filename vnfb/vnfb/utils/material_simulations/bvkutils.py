cec49184454a7959023aa712417ad0a86f369172
Jiao Lin <linjiao@caltech.edu>
2009-12-16 21:03:04 +0000

    vnf-luban:utils....bvk: moved the bulk of method "findForceContantTensorConstraints" to 'bvk' package
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@1481 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/utils/material_simulations/bvkutils.py
+++ vnfb/vnfb/utils/material_simulations/bvkutils.py
@@ -12,62 +12,7 @@
 #
 
 
-# helpers for bvk. probably should move some of them to the bvk package
-#  
-#  * depends on sympy, numpy
-#
-
-
-import numpy
-
-import sympy.matrices as matrices
-import sympy
-def _matrixelementsymbol(i,j):
-    r = sympy.Symbol('m%s%s' % (i+1,j+1))
-    return r
-T = [ [_matrixelementsymbol(i,j) for j in range(3)] for i in range(3)]
-T = matrices.Matrix(T)
-
-
-def symmetryRestricted3X3Tensor(R):
-    """find the restrictions to a 3X3 tensor given the transformation matrix
-    of the symmetry operation.
-    """
-    R = matrices.Matrix(R)
-    # R-1 * T * R = T
-    equation = R.T * T * R - T
-    from sympy.solvers import solve
-    import operator
-    equation = reduce(operator.add, equation.tolist())
-    symbols = reduce(operator.add, T.tolist())
-    solution = solve(equation, *symbols)
-    return solution
-
-
-def symmetriesRestricted3X3Tensor(Rs):
-    """find the restrictions to a 3X3 tensor given the transformation matrices
-    of the symmetry operations.
-    """
-    equations = _equationsForSymmetriesRestricted3X3Tensor(Rs)
-
-    import operator
-    symbols = reduce(operator.add, T.tolist())
-
-    from sympy.solvers import solve
-    solution = solve(equations, *symbols)
-    
-    return solution
-
-
-def _equationsForSymmetriesRestricted3X3Tensor(Rs):
-    Rs = map(matrices.Matrix, Rs)
-    # R-1 * T * R = T
-    equations = [R.T * T * R - T for R in Rs]
-    import operator
-    equations = [reduce(operator.add, equation.tolist())
-                 for equation in equations]
-    return reduce(operator.add, equations)
-    
+# depends on "bvk", "matter"
 
 # depends on "matter" package
 def findForceContantTensorConstraints(vector, lattice, sg):
@@ -79,54 +24,20 @@ def findForceContantTensorConstraints(vector, lattice, sg):
     lattice is an instance of matter.Lattice.Lattice. required attribute: 'stdbase', which is the matrix of base vectors in cartesian coordinates in standard orientation.
     vector is a vector of 3 in relative lattice coords
     """
-    b = lattice.stdbase
-    import numpy.linalg as nl
-    binv = nl.inv(b)
-
-    # the equations result from symmetry requirement of the bond
-    # the rotation matrices in relative coords
-    #   R in Rs don't work: special things about numpy array
-    Rs = []; ids = []
-    for symop in sg.iter_symops_leave_vector_unchanged(vector):
-        R = symop.R
-        if id(R) not in ids:
-            Rs.append(R)
-            ids.append(id(R))
-        continue
-    #  function to convert matrix to cartesian
-    def _toCartesian(symop):
-        return numpy.dot(numpy.dot(binv, R), b)
-    Rs = map(_toCartesian, Rs)
-    equations = _equationsForSymmetriesRestricted3X3Tensor(Rs)
-
-    # add the equations for instrisic symmetry of force constant tensor
-    equations += _intrinsicSymmetryEquationsForForceConstantTensor
-
-    # the symbols to solve (9 matrix elements)
-    import operator
-    symbols = reduce(operator.add, T.tolist())
-
-    # solve the equations
-    from sympy.solvers import solve
-    restrictions = solve(equations, *symbols)
     
+    from bvk.find_force_constant_tensor_constraints \
+         import findForceContantTensorConstraints as find
+    restrictions = find(vector, lattice, sg)
+
     # need to convert restrictions dictionary to descriptor like
     _ = SympyExpression2ConstraintExpression()
     constraints = []
-    for symbol, expression in restrictions.iteritems():
+    for symbol, expression in restrictions:
         # print 'sympy:', symbol, '=', expression
         var = _.render(symbol)
         expression = _.render(expression)
         constraints.append( var == expression )
     return constraints
-
-
-# intrinsic symmetry of force constant tensor
-_intrinsicSymmetryEquationsForForceConstantTensor = [
-    T[0,1]-T[1,0],
-    T[0,2]-T[2,0],
-    T[1,2]-T[2,1],
-    ]
 
 
 # visitor to sympy expression to construct vnfb.utils.constraints expression
@@ -192,57 +103,6 @@ class VarFromSympySymbol(constraints.Variable):
 
 import unittest
 class TestCase(unittest.TestCase):
-
-    def test1(self):
-        R = [
-            [0, 1, 0],
-            [1, 0, 0],
-            [0, 0, -1],
-            ]
-        restrictions = symmetryRestricted3X3Tensor(R)
-        assert restrictions[T[0,0]] == T[1,1]
-        assert restrictions[T[0,1]] == T[1,0]
-        assert restrictions[T[2,0]] == -T[2,1]
-        assert restrictions[T[0,2]] == -T[1,2]
-        return
-
-
-    def test2(self):
-        R = [
-            [0, 1, 0],
-            [1, 0, 0],
-            [0, 0, -1],
-            ]
-        restrictions = symmetriesRestricted3X3Tensor([R])
-        assert restrictions[T[0,0]] == T[1,1]
-        assert restrictions[T[0,1]] == T[1,0]
-        assert restrictions[T[2,0]] == -T[2,1]
-        assert restrictions[T[0,2]] == -T[1,2]
-        return
-
-
-    def test3(self):
-        R1 = [
-            [0, 1, 0],
-            [1, 0, 0],
-            [0, 0, -1],
-            ]
-        R2 = [
-            [1, 0, 0],
-            [0, 0, 1],
-            [0, -1, 0],
-            ]
-        restrictions = symmetriesRestricted3X3Tensor([R1, R2])
-        self.assertEqual(restrictions[T[0,0]], T[2,2])
-        self.assertEqual(restrictions[T[1,1]], T[2,2])
-        self.assertEqual(restrictions[T[0,1]], 0)
-        self.assertEqual(restrictions[T[0,2]], 0)
-        self.assertEqual(restrictions[T[1,0]], 0)
-        self.assertEqual(restrictions[T[1,2]], 0)
-        self.assertEqual(restrictions[T[2,0]], 0)
-        self.assertEqual(restrictions[T[2,1]], 0)
-        return
-
 
     def test4(self):
         # fcc
