49c0016beac1f47255c7bd42db009f6a0d350983
Jiao Lin <linjiao@caltech.edu>
2009-12-18 23:19:40 +0000

    vnf-luban: 1. show "base" in view of lattice 2. show "structure" in view of bvkmodel
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@1510 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/dom/Lattice.py
+++ vnfb/vnfb/dom/Lattice.py
@@ -23,7 +23,7 @@ LatticeTable = o2t(Lattice)
 
 # view
 def customizeLubanObjectDrawer(self, drawer):
-    drawer.mold.sequence = ['a', 'b', 'c', 'alpha', 'beta', 'gamma']
+    drawer.mold.sequence = ['a', 'b', 'c', 'alpha', 'beta', 'gamma', 'base']
 Lattice.customizeLubanObjectDrawer = customizeLubanObjectDrawer
 
 
