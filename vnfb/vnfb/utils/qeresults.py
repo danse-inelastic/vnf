98455e8d03c1878af81ed607d44e714115b053c9
Alexander Dementsov <dexity@gmail.com>
2009-12-20 23:34:47 +0000

    Attempt to handle change of states correctly
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@1516 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/utils/qeresults.py
+++ vnfb/vnfb/utils/qeresults.py
@@ -11,35 +11,68 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+from vnfb.utils.qestatus import QEStatus
+from vnf.applications.PackJobDir import PackJobDir
+
 class QEResults:
-    """Handles complexity of results of simulation tasks"""
+    """
+    Handles complexity of job results
+    There are the following supported states:
+        norequest   - No request has been sent for packing the results
+        started     - Packing has just started
+        packing     - Packing on progress
+        oldrequest  - Outdated request
+        packingagain    - Packing started again
+    """
 
     def __init__(self, director, job):
         self._director  = director
         self._job       = job   # not None
+        self._status    = QEStatus()
 
 
     def retrieve(self):
-        "Retrieves results"
-        link    = lc.link()
+        "Retrievs results based on the status"
 
-        ptrfilepath = self._ptrfilepath()
+        self.status()   # Set status
 
-        # If pointer file does not exists, need to start packing
-        # If it exists the file will not be delivered again
-        if not os.path.exists(ptrfilepath):
+        if self._status in ["norequest", "oldrequest"]:
             self._startPacking()
-            link.label  = "Started Packing"
-            return link
+            return self.statusstring()
 
-        # if packing is in process, say that
-        s = open(ptrfilepath).read()
-        if s == PackJobDir.PACKINGINPROCESS:
-            link.label  = "Packing In Progress"
-            return link
 
 
         self._untar()
+
+
+
+        return string
+
+
+    def status(self):
+        "Returns status of the simulation without any action (such as results retrieval)"
+
+        # Status changed during results retrieval
+        if self._status.get() in [""]:
+            return 
+
+        ptrfilepath = self._ptrfilepath()
+
+        # Packing was not requested
+        if not os.path.exists(ptrfilepath):
+            self._status.set("norequest", "Not Requested")
+            return self.statusstring()
+
+        # Packing in progress
+        s = open(ptrfilepath).read()
+        if s == PackJobDir.PACKINGINPROCESS:
+            self._status.set("packing", "Packing In Progress")
+            return self.statusstring()
+
+        # Packing is old
+        # if tarball old, 
+        #    self._status.set("oldrequest", "Outdated Request")
+        #    -> "Packing Again"
 
         # Keep!
 #        server      = director.clerk.getServers(id = sim.serverid)
@@ -52,13 +85,28 @@ class QEResults:
 #            return link
 
 
-        return link
+        s       = open(ptrfilepath).read()
+        ss      = s.strip("/")
+        if len(ss) != 0 and ss[0] == "tmp":
+            return self._tarlink(ptrfilepath)
 
 
-    def status(self):
-        "Returns status of the simulation without results retrieval"
-        link = self._tarlink(ptrfilepath)
-        return
+        return self.statusstring()
+
+
+    def _norequest(self, ptrfilepath):
+        "Packing was not requested"
+        if not os.path.exists(ptrfilepath):
+            return True
+
+        return False
+        
+
+    def _packing(self, ptrfilepath):
+
+
+    def statusstring(self):
+        return self._status.string()
 
 
     def _tarlink(self, ptrfilepath):
@@ -66,9 +114,10 @@ class QEResults:
         f           = open(ptrfilepath)
         localpath   = f.read().strip()
         path        = "tmp/%s" % localpath      # Example: "tmp/tmp31LUyu/44MTMA42.tgz"
-        link        = HtmlDocument(text="<a href='%s'>%s</a>" % (path, text) )
-
-        return link
+        self._status.setHtmlLink(text, path)
+        #link        = HtmlDocument(text="<a href='%s'>%s</a>" % (path, text) )
+        #return link
+        return self._status.string("html")
 
 
     def _ptrfilepath(self):
@@ -87,8 +136,43 @@ class QEResults:
     def _startPacking(self):
         from vnf.components.Job import pack
         pack(self._job, self._director, debug=False)
+        
+        if self._status.get() == "oldrequest":
+            # Packing is repeated again
+            self._status.set("packingagain", "Packing Again")
+        else:
+            # Packing has just started
+            self._status.set("packing", "Started Packing")
+        
 
 
 __date__ = "$Dec 20, 2009 11:36:09 AM$"
+
+
+
+
+# ********************** DEAD CODE **************************
+
+#        #link    = lc.link()
+#        #ptrfilepath = self._ptrfilepath()
+#
+#        # If pointer file does not exists, need to start packing
+#        # If it exists the file will not be delivered again
+#        if not os.path.exists(ptrfilepath):
+#            self._startPacking()
+#            self._status.set("Started Packing")
+#            return self._status.string()
+
+#            #link.label  = "Started Packing"
+#            #return link
+#
+#        # if packing is in process, say that
+#        s = open(ptrfilepath).read()
+#        if s == PackJobDir.PACKINGINPROCESS:
+#            self._status.set("Packing In Progress")
+#            return self._status.string()
+
+            #link.label  = "Packing In Progress"
+            #return link
 
 
--- vnfb/vnfb/utils/qestatus.py
+++ vnfb/vnfb/utils/qestatus.py
@@ -11,8 +11,84 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+import luban.content as lc
+
 class QEStatus:
-    "Data structure for job status"
+    """
+    Data structure for job and delivery status"
+    """
+    def __init__(self, message=""):
+        self._message   = message
+        self._onload    = None
+        self._htmllink  = ""    # for HtmlDocument
+        self._islink    = False
+        self._class     = ""
+        self._state     = ""    # State of the status
+
+
+    def string(self, format=None):
+        """
+        Returns formatted status message
+
+        Parameters:
+            format  - format in which to display the message
+                None    - No format
+                "html"  - luban HtmlDocument
+                "link"  - luban Link (action link)
+                "paragraph" - luban Paragraph
+        """
+        
+        if format == "link"
+            return self._link()
+
+        if format == "html":
+            return self._html()
+
+        if format == "paragraph":
+            return self._paragraph()
+
+        return self._message
+
+
+    def setHtmlLink(self, message, link, Class=None):
+        self._message   = message
+        self._htmllink  = link
+        self._class     = Class
+
+
+    def setLink(self, message, onload = None, Class = None):
+        self._message   = message
+        self._onload    = onload
+        self._class     = Class
+
+    
+    def message(self, message):
+        "Set status message"
+        self._message   = message
+        
+
+    def set(self, state, message=None):
+        "Sets state of the status"
+        self._state     = state
+        if message:
+            self._message   = message
+
+
+    def get(self, state):
+        "Returns state of the status"
+        return self._state
+
+    def _html(self):
+        return lc.htmldocument(text="<a href='%s'>%s</a>" % (self._htmllink, self._message))
+
+
+    def _link(self):
+        return lc.link(label=self._message, onload=self._onload, Class=self._class)
+
+
+    def _paragraph(self):
+        return lc.paragraph(text=self._message, Class=self._class)
+
 
 __date__ = "$Dec 20, 2009 12:06:28 PM$"
 
