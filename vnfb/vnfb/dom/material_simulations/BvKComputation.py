6ee2c2df96615f34621d8a2bdec63ae2439b4946
Jiao Lin <linjiao@caltech.edu>
2009-12-25 16:21:15 +0000

    vnf-luban: view of "bvk_getdos" computation
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@1574 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/dom/material_simulations/BvKComputation.py
+++ vnfb/vnfb/dom/material_simulations/BvKComputation.py
@@ -26,11 +26,18 @@ from BvKModel import BvKModel
 
 class BvKComputation(object):
 
+    model = None
+    matter = None
+
     class Inventory(InvBase):
         
         matter = InvBase.d.reference(name='matter', targettype=None, targettypes=[Structure], owned=0)
         model = InvBase.d.reference(name='model', targettype = BvKModel, owned=0)
+
         
+    def isConfigured(self):
+        if self.model is None or self.matter is None: return False
+        return True
 
 
 # name of classes must use convention BvK_Get..., see method "getComputationClass"
@@ -42,10 +49,18 @@ class BvK_GetDos(BvKComputation):
 
     class Inventory(BvKComputation.Inventory):
 
-        df = InvBase.d.float(name='df', default = 0.5) # unit THz
+        df = InvBase.d.float(name='df', default = 0.1) # unit THz
         df.tip = 'Frequency axis bin size. unit: THz'
         N1 = InvBase.d.int(name='N1', default = 10)
         N1.tip = 'number of sampling points in Q space(in 1 dimension)'
+
+
+    def isConfigured(self):
+        c = super(BvK_GetDos, self).isConfigured()
+        if not c: return c
+
+        if self.df <= 0 or self.N1 <= 0: return False
+        return True
 
     
     def customizeLubanObjectDrawer(self, drawer):
