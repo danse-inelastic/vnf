3f7788f2e5afb9bd88fd4c0964924faac5a90610
Jiao Lin <linjiao@caltech.edu>
2009-12-27 21:07:10 +0000

    vnf-luban: added view of computed phonon dos to a load-on-demand section of "atomicstructure" overview.
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@1581 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/dom/material_simulations/PhononDOS.py
+++ vnfb/dom/material_simulations/PhononDOS.py
@@ -44,37 +44,6 @@ PhononDOSTable.datafiles = [
     ]
 
 
-# view
-def customizeLubanObjectDrawer(self, drawer):
-    def createReadOnlyView(obj, editlink=None):
-        # the document to build
-        import luban.content as lc
-        doc = lc.document(title='Phonon DOS')
-        
-        # the idf file
-        path = drawer.obj_resource_path
-        import os
-        f = os.path.join(path, 'data.idf')
-        
-        # check path
-        if not os.path.exists(f): return
-
-        # read
-        from idf import DOS
-        (t,version,comment), e, i = DOS.read(f)
-
-        # plot
-        p = lc.plot2d(width=480, height=320)
-        p.curve(x=list(e), y=list(i), label='Density of states')
-
-        #
-        doc.add(p)
-        
-        return doc
-    drawer.createReadOnlyView = createReadOnlyView
-    return
-PhononDOS.customizeLubanObjectDrawer = customizeLubanObjectDrawer
-
 
 # version
 __id__ = "$Id$"
