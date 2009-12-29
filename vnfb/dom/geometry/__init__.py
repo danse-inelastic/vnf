4d180cc54b2838922cd2fa6ec7c8d3d9ec1bd1f5
Jiao Lin <linjiao@caltech.edu>
2009-12-29 03:02:06 +0000

    vnf-luban:vnfb.dom.geometry: started
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@1594 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ vnfb/dom/geometry/__init__.py
@@ -0,0 +1,20 @@
+# -*- Python -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                      (C) 2006-2010  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+from AbstractShape import AbstractShape
+
+
+# version
+__id__ = "$Id$"
+
+# End of file 
