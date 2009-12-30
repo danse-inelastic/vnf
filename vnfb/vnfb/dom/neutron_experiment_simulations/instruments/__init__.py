801f463ce2d837f285e7574235a01133fe6aa6eb
Jiao Lin <linjiao@caltech.edu>
2009-12-30 05:54:23 +0000

    vnfb.dom.neutron_experiment_simulations.instruments: start migrating from vnf-alpha
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@1611 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ vnfb/vnfb/dom/neutron_experiment_simulations/instruments/__init__.py
@@ -0,0 +1,42 @@
+# -*- Python -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                        (C) 2008  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+thispackage = 'vnfb.dom.instruments'
+
+instruments = [
+    'ARCS_beam',
+    'IdealPowderINS',
+    'SANS_NG7',
+    'SEQUOIA',
+    'Pharos',
+    'Powgen3',
+    'VULCAN',
+    'Test',
+    ]
+
+
+def initall(db):
+    modules = [_import('%s.%s' % (thispackage, name)) for name in instruments]
+    for m in modules: m.create(db)
+    return
+    
+
+def _import(package):
+    return __import__(package, {}, {}, [''])
+
+
+# version
+__id__ = "$Id$"
+
+# End of file 
