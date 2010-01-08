c29615eba9f8284e4f2fc9f11fba7c007ce87c0b
J Brandon Keith <jbrkeith@gmail.com>
2010-01-08 14:12:33 +0000

    adding motionDosCalc as first analysis type...about 15 more to go...
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@1714 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ vnfb/vnfb/dom/analysis/computation_types.py
@@ -0,0 +1,32 @@
+# -*- Python -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                      (C) 2006-2009  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+typenames = [
+    'analysis.BvKComputation.BvK_GetDos',
+    'material_simulations.BvKComputation.BvK_GetDispersion',
+    'material_simulations.GulpSettingsHolder.GulpSettings',
+    ]
+
+
+#deps_typenames = [
+#    'material_simulations.PhononDOS.PhononDOS',
+#    'material_simulations.PhononDispersion.PhononDispersion',
+#    'material_simulations.SQE.SQE',
+#    ]
+
+
+# version
+__id__ = "$Id$"
+
+# End of file 
