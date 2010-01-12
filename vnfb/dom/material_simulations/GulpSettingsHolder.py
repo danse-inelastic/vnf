d139149277e5313a3b93a743c33f3908e0c57f34
J Brandon Keith <jbrkeith@gmail.com>
2010-01-12 00:09:15 +0000

    switching GulpSettings back to Computation interface (rather than Material Simulation) because want to keep the matter reference in the DO so scripters can assign structures, for example
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@1761 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/dom/material_simulations/GulpSettingsHolder.py
+++ vnfb/dom/material_simulations/GulpSettingsHolder.py
@@ -15,13 +15,13 @@ from _ import o2t
 
 # still import from vnf-alpha dom. need to change
 #from vnfb.dom.Computable import Computable
-#from vnfb.dom.Computation import Computation
-from vnfb.dom.MaterialSimulation import MaterialSimulation
+from vnfb.dom.Computation import Computation
+#from vnfb.dom.MaterialSimulation import MaterialSimulation
 
 #from dsaw.model.Inventory import Inventory as InvBase
 
 from memd.gulp.GulpSettings import GulpSettings
-GulpSettingsHolder = o2t(GulpSettings, {'subclassFrom': MaterialSimulation, 'dbtablename':'gulpsettings'})
+GulpSettingsHolder = o2t(GulpSettings, {'subclassFrom': Computation, 'dbtablename':'gulpsettings'})
 GulpSettingsHolder.job_builder = 'material_simulations/gulpSettings'
 GulpSettingsHolder.actor = 'material_simulations/gulpSettings'
 GulpSettingsHolder.result_retriever = 'material_simulations/gulpSettings'
