f0dd6901ffccba4c9161b83b6dba3ae94f7f426d
Jiao Lin <linjiao@caltech.edu>
2010-01-21 23:12:01 +0000

    vnf documentation: change from the default sphinx style to a lighter color-scheme. more to come
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@1933 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ vnfb/docs/Make.mm
@@ -0,0 +1,63 @@
+# -*- Makefile -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Michael A.G. Aivazis
+#                        California Institute of Technology
+#                        (C) 1998-2008  All Rights Reserved
+#
+# <LicenseText>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+PROJECT = vnf
+PACKAGE = sphinx
+
+
+RECURSE_DIRS = \
+
+
+#--------------------------------------------------------------------------
+#
+
+all: docs
+	BLD_ACTION="all" $(MM) recurse
+
+PROJ_CLEAN = \
+	_build/* \
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+docs: export-data sphinx-build 
+
+
+include std-docs.def
+
+SPHINX_BUILD_TMP = _build/html
+
+RSYNC_A = rsync -a
+sphinx-build: Makefile $(EXPORT_DOCDIR)
+	make html
+	$(RSYNC_A) $(SPHINX_BUILD_TMP)/ $(EXPORT_DOCDIR)/
+
+
+EXPORT_DATADIRS = \
+	_static \
+
+
+export-data: $(EXPORT_DATADIRS) $(SPHINX_BUILD_TMP)
+	for x in $(EXPORT_DATADIRS); do { \
+	  $(RSYNC_A) $$x/ $(SPHINX_BUILD_TMP)/$$x/; \
+	} done
+
+# version
+# $Id: Make.mm,v 1.2 2008-04-13 03:55:58 aivazis Exp $
+
+# End of file
