3d694bbcadbf958557f492388168d4f996e28724
Jiao Lin <linjiao@caltech.edu>
2010-01-25 23:36:57 +0000

    neutronplayer component editor: show brief info of neutron storage.
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@1988 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/dom/neutron_experiment_simulations/neutron_components/NeutronPlayer.py
+++ vnfb/dom/neutron_experiment_simulations/neutron_components/NeutronPlayer.py
@@ -21,8 +21,65 @@ class NeutronPlayer(base):
         drawer.mold.sequence = [
             'componentname', 'short_description',
             'referencename', 'position', 'orientation',
+            'neutrons',
             ]
-        drawer.sequence = ['properties', 'neutrons']
+        drawer.sequence = ['properties']
+
+        def _createfield_for_neutrons(obj):
+            # this is a method of mold.
+            self = drawer.mold
+
+            # imports
+            import luban.content as lc
+            from luban.content import load, select
+            from luban.content.FormSelectorField import FormSelectorField
+            
+            # utils
+            orm = self.orm
+
+            # data 
+            record = self.orm(obj)
+            referred_record = record.neutrons and record.neutrons.id \
+                              and record.neutrons.dereference(self.orm.db)
+
+            # widget
+            doc = lc.document(Class='container', id='neutrons-selector-container')
+            sp = doc.splitter(orientation='horizontal')
+            left = sp.section(); right = sp.section()
+            #
+            selector = FormSelectorField(label='Neutrons:', name='neutrons')
+            left.add(selector)
+            #
+            viewcontainer = right.document(Class='container')
+            #
+            loadview = lambda uid: load(
+                actor='orm/neutronstorages', routine='createGraphicalView',
+                uid=uid, start=0, end=2)
+
+            # default selection
+            if referred_record:
+                value=orm.db.getUniqueIdentifierStr(referred_record)
+                viewcontainer.oncreate = select(element=viewcontainer).append(
+                    loadview(value))
+            else:
+                value=None
+
+            # choices
+            #  dynamically load choices
+            entries = load(
+                actor='orm/neutronplayers',
+                routine='getSelectorEntriesForReference',
+                refname = 'neutrons',
+                include_none_entry = 1,
+                )
+            selector.oncreate = select(element=selector).setAttr(entries=entries, value=value)
+            selector.onchange = select(element=viewcontainer).replaceContent(
+                loadview(select(element=selector).getAttr('value')))
+            
+            return doc
+
+        drawer.mold._createfield_for_neutrons = _createfield_for_neutrons
+        
     pass
 
 
