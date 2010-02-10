6376abce9c3b9b5230d698bcac8c5339016e0c1e
Jiao Lin <linjiao@caltech.edu>
2010-02-10 06:14:09 +0000

    experiment table: refactored a bit
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@2067 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/content/tables/Make.mm
+++ vnfb/vnfb/content/tables/Make.mm
@@ -36,6 +36,7 @@ all: export
 
 EXPORT_PYTHON_MODULES = \
 	atomicstructure.py \
+	experiment.py \
 	materialsimulation.py \
 	sample.py \
 	selectoneatomicstructure.py \
---
+++ vnfb/vnfb/content/tables/experiment.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                  Jiao Lin
+#                     California Institute of Technology
+#                       (C) 2009  All Rights Reserved
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+from luban.content.table import Model, View, Table
+from luban.content import load
+from luban.content.Link import Link
+
+class model(Model):
+
+    selected = Model.descriptors.bool(name='selected')
+    id = Model.descriptors.link(name='id')
+    description = Model.descriptors.str(name='description')
+
+    row_identifiers = ['id']
+
+
+columns = [
+    View.Column(label='', measure='selected'),
+    View.Column(label='ID', measure='id'),
+    View.Column(label='Description', measure='description', editable=True),
+    ]
+
+def view(cols, editable=True):
+    global columns
+    columns = filter(lambda col: col.measure in cols, columns)
+    return View(columns=columns, editable=True)
+
+
+def getSelected(matter): return False
+def getId(experiment):
+    label = experiment.id
+    link = Link(
+        label = label,
+        onclick = load(
+            actor='experiment', routine='showExperimentView',
+            id = experiment.id,
+            )
+        )
+    return link
+def getDescription(experiment):
+    return experiment.short_description
+
+
+
+def table(experiments, cols, director, editable=True):
+    global view
+    view = view(cols, editable=editable)
+    
+    import operator
+    value_generators = [
+        eval('get'+col.measure[0].upper()+col.measure[1:])
+        for col in view.columns]
+    record2tuple = lambda record: [g(record) for g in value_generators]
+    data = map(record2tuple, experiments)
+    
+    table = Table(model=model, data=data, view=view, id='experiment-table')
+    return table
+
+
+# version
+__id__ = "$Id$"
+
+# End of file 
