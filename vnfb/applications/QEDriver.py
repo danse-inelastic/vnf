2a4e88b2dfdd5ee06bdb4c26485c10d061fbdbb8
Alexander Dementsov <dexity@gmail.com>
2010-03-04 15:27:28 +0000

    Having some strange problem
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@2151 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/applications/QEDriver.py
+++ vnfb/applications/QEDriver.py
@@ -16,10 +16,10 @@
 
 #import time
 #
-#from vnfb.dom.QEJob import QEJob
-#from vnfb.utils.qeutils import makedirs, writefile, stamp
-#from vnfb.utils.qeconst import STATES, RUNSCRIPT, TYPE, NOPARALLEL
-#from vnfb.utils.qeutils import packname, unpackname
+from vnfb.dom.QEJob import QEJob
+from vnfb.utils.qeutils import makedirs, writefile, stamp
+from vnfb.utils.qeconst import STATES, RUNSCRIPT, TYPE, NOPARALLEL
+from vnfb.utils.qeutils import packname, unpackname
 from luban.applications.UIApp import UIApp as base
 
 import pyre.idd
@@ -43,7 +43,7 @@ class QEDriver(base):
         taskid      = pyre.inventory.str('taskid', default='')
         subtype     = pyre.inventory.str('subtype', default='')
         
-        idd = pyre.inventory.facility('idd-session', factory=pyre.idd.session, args=['idd-session'])
+        idd = pyre.inventory.facility('idd-session', factory=pyre.idd.session, args=['idd-session',])
         idd.meta['tip'] = "access to the token server"
 
         clerk = pyre.inventory.facility(name="clerk", factory=vnfb.components.clerk)
@@ -57,15 +57,15 @@ class QEDriver(base):
 
 
     def main(self):
-
-        open("/tmp/qedriver.txt", "w").write("Hi")
-
+        open("/tmp/qedriver.txt", "w").write("Hello")
 #        sim = self.clerk.getQESimulations(id = self.id)
 #        sim.setClerk(self.clerk)
 #        sim.updateRecord({"label": "Hi",})
 
+#        self.submitJob()
 
-#    def submitJob(self, director):
+
+#    def submitJob(self):
 #        """
 #        Submit simulation job
 #        The process of submission of simulation includes the following steps:
@@ -73,52 +73,53 @@ class QEDriver(base):
 #        2. Copy files to the computational cluster
 #        3. Submit Job
 #        """
-#
-#
-#        self._createJob(director)
-#        self._storeFiles(director)
-#        self._moveFiles(director)
-#        self._scheduleJob(director)
-#
-#
-#    def _createJob(self, director):
+
+#        self._createJob()
+#        self._storeFiles()
+#        self._moveFiles()
+#        self._scheduleJob()
+
+
+#    def _createJob(self):
 #        "Create Job"
-#        self._sim   = director.clerk.getQESimulations(id = self.id)     # Should exist
-#        settings    = director.clerk.getQESettings(where = "simulationid='%s'" % self.id)   # Should exist
+        
+#        sim = self.clerk.getQESimulations(id = self.id)
+#        self._sim   = self.clerk.getQESimulations(id = self.id)     # Should exist
+#        settings    = self.clerk.getQESettings(where = "simulationid='%s'" % self.id)   # Should exist
 #        setting     = settings[0]
 #        params  = {"taskid":        self.taskid,
 #                   "serverid":      self._sim.serverid,  # -> take from QESimulations
 #                   "status":        "Submitted",    # Fixed status
 #                   "timesubmitted": stamp(),
-#                   "creator":       director.sentry.username,
+#                   "creator":       self.sentry.username,
 #                   "numberprocessors":   setting.numproc, # -> take from QESettings
 #                   "description":   self.subtype
 #                   }
-#
-##        debug   = False  # Debugging flag
-##        if debug:
-##            #self.taskid = "4FUNHMTP"
-##            self.taskid = "46M3E9PE"
-##            jobs        = director.clerk.getQEJobs(where = "taskid='%s'" % self.taskid) # Temp
-##            self._job   = jobs[0]
-##        else:
-#
+
+#        debug   = False  # Debugging flag
+#        if debug:
+#            #self.taskid = "4FUNHMTP"
+#            self.taskid = "46M3E9PE"
+#            jobs        = self.clerk.getQEJobs(where = "taskid='%s'" % self.taskid) # Temp
+#            self._job   = jobs[0]
+#        else:
+
 #        self._job  = QEJob()
-#        self._job.setDirector(director)
-#        self._job.createRecord(params) # uncomment
-#
-#
-#    def _storeFiles(self, director):
+#        self._job.setDirector(self)
+#        #self._job.createRecord(params)
+
+
+#    def _storeFiles(self):
 #        """TEMP SOLUTION: Stores files from configuration input strings """
-#        self._storeConfigurations(director)
-#        self._createRunScript(director)
+#        self._storeConfigurations()
+#        self._createRunScript()
 #        self._prepareFiles()
 #
 #
-#    def _storeConfigurations(self, director):
+#    def _storeConfigurations(self):
 #        "Store Configuration files"
-#        inputs  = director.clerk.getQEConfigurations(where = "taskid='%s'" % self.taskid)
-#        dds     = director.dds
+#        inputs  = self.clerk.getQEConfigurations(where = "taskid='%s'" % self.taskid)
+#        dds     = self.dds
 #        for input in inputs:
 #            fn          = input.filename
 #            pfn         = packname(input.id, fn)                # E.g. 44XXJJG2ni.scf.in
@@ -128,11 +129,11 @@ class QEDriver(base):
 #            self._files.append(pfn)
 #
 #
-#    def _createRunScript(self, director):
-#        settingslist = director.clerk.getQESettings(where = "simulationid='%s'" % self.id)       # not None
-#        inputs      = director.clerk.getQEConfigurations(where = "taskid='%s'" % self.taskid)
-#        task        = director.clerk.getQETasks(id = self.taskid)
-#        server      = director.clerk.getServers(id = self._job.serverid)
+#    def _createRunScript(self):
+#        settingslist = self.clerk.getQESettings(where = "simulationid='%s'" % self.id)       # not None
+#        inputs      = self.clerk.getQEConfigurations(where = "taskid='%s'" % self.taskid)
+#        task        = self.clerk.getQETasks(id = self.taskid)
+#        server      = self.clerk.getServers(id = self._job.serverid)
 #        settings    = settingslist[0]
 #        input       = inputs[0]
 #
@@ -149,13 +150,13 @@ class QEDriver(base):
 #
 #        # QE temp simulation directory is qesimulations/[simid] directory
 #        # E.g.: /home/dexity/espresso/qesimulations/3YEQ8PNV    -> no trailing slash
-#        qetempdir  = director.dds.abspath(self._sim, server=server)
+#        qetempdir  = self.dds.abspath(self._sim, server=server)
 #        cmds    = [ "#!/bin/env bash",   # Suppose there is bash available
 #                    "export ESPRESSO_TMPDIR=%s/" % qetempdir,
 #                    " ".join(words)
 #        ]
 #
-#        dds     = director.dds
+#        dds     = self.dds
 #        self._write2file(dds, self._job, RUNSCRIPT, "\n".join(cmds))    # -> qejobs directory
 #        dds.remember(self._job, RUNSCRIPT)  # Important step during which the .__dds_nodelist* files are created
 #        self._files.append(RUNSCRIPT)
@@ -180,7 +181,7 @@ class QEDriver(base):
 #        writefile(absfilename, content)
 #
 #
-#    def _moveFiles(self, director):
+#    def _moveFiles(self):
 #        """
 #        Moves files from local server to the computational cluster.
 #        Files that need to be moved:
@@ -190,26 +191,26 @@ class QEDriver(base):
 #        Notes:
 #            - See also: submitjob.odb
 #        """
-#        dds     = director.dds
-#        server  = director.clerk.getServers(id = self._job.serverid)
+#        dds     = self.dds
+#        server  = self.clerk.getServers(id = self._job.serverid)
 #        dds.make_available(self._job, server=server, files=self._files)
 #
 #        # Create output directory (ESPRESSO_TEMPDIR) for QE
 #        dds.makedirs(self._sim, server=server)
 #
 #
-#    def _test_makedirs(self, director):
-#        dds         = director.dds
-#        self._sim   = director.clerk.getQESimulations(id = self.id)
-#        server      = director.clerk.getServers(id = self._sim.serverid)
+#    def _test_makedirs(self):
+#        dds         = self.dds
+#        self._sim   = self.clerk.getQESimulations(id = self.id)
+#        server      = self.clerk.getServers(id = self._sim.serverid)
 #        dds.makedirs(self._sim, server=server)
 #
 #
-#    def _scheduleJob(self, director):
+#    def _scheduleJob(self):
 #        "Schedule job"
-#        dds     = director.dds
+#        dds     = self.dds
 #        from vnfb.utils.qescheduler import schedule
-#        schedule(self._sim, director, self._job)
+#        schedule(self._sim, self, self._job)
 
 
     def __init__(self):
@@ -235,5 +236,11 @@ class QEDriver(base):
 
 
 __date__ = "$Mar 3, 2010 11:04:10 PM$"
+
+
+#        sim = self.clerk.getQESimulations(id = self.id)
+#        sim.setClerk(self.clerk)
+#        sim.updateRecord({"label": "Hi",})
+
 
 
