ffbfbce86a80830d58cad4be68e54be4dde8b4b2
Jiao Lin <linjiao@caltech.edu>
2010-03-08 22:46:03 +0000

    DetectorSystem_fromXML: separate the system core (hierarchy) and more configurable parameters; added ARCS detector system hierarchy record in initdb
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@2168 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/dom/neutron_experiment_simulations/neutron_components/_.py
+++ vnfb/vnfb/dom/neutron_experiment_simulations/neutron_components/_.py
@@ -1,3 +1,4 @@
 from vnfb.dom._ import o2t
+from vnfb.dom.AbstractOwnedObjectBase import AbstractOwnedObjectBase
 from AbstractNeutronComponent import NeutronComponentTableBase
 from Monitor import Monitor, MonitorTableBase
