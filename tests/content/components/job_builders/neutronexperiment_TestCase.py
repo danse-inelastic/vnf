230df7029481212a3f67334d95708cccddca5c16
Jiao Lin <linjiao@caltech.edu>
2010-03-09 16:59:06 +0000

    more tests for detectorsystem_fromxml.
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@2172 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- tests/content/components/job_builders/neutronexperiment_TestCase.py
+++ tests/content/components/job_builders/neutronexperiment_TestCase.py
@@ -50,12 +50,22 @@ class TestApp(base):
         self.testFacility = testFacility
 
         #
-        computation = self.domaccess.getExperimentRecord(expid)
-        db = self.domaccess.db
-        job = computation.getJob(db)
+        self.computation = self.domaccess.getExperimentRecord(expid)
+        db = self.db = self.domaccess.db
+        self.job = self.computation.getJob(db)
 
+        self.buildjob()
+        self.submitjob()
+        return
+
+
+    def buildjob(self):
         # path
         dds = self.dds
+        job = self.job
+        db = self.db
+
+        #
         path = dds.abspath(job)
 
         # remove the directory if it exists
@@ -63,13 +73,77 @@ class TestApp(base):
             import shutil
             shutil.rmtree(path)
 
+        # remove the deps if it exist
+        job.dependencies.clear(db)
+
         # build job
         from vnfb.utils.job import buildjob
+        computation = self.computation
         buildjob(computation, db=db, dds=dds, path=path, director=self)
 
         # confirm
         # testFacility.assert_(os.path.exists(os.path.join(path, 'system')))
         # testFacility.assert_(os.path.exists(os.path.join(path, 'run.sh')))
+        return
+
+
+    def submitjob(self):
+        "test job submission"
+
+        testFacility = self.testFacility
+        db = self.db
+        dds = self.dds
+        job = self.job
+        
+        # remove the directory if it exists
+        path = dds.abspath(job)
+        if os.path.exists(path):
+            import shutil
+            shutil.rmtree(path)
+
+        # remove the deps if it exist
+        job.dependencies.clear(db)
+
+        # mark job as just created
+        job.state = 'created'
+        db.updateRecord(job)
+
+        # find the itask
+        from vnfb.dom.ITask import ITask
+        gp = job.globalpointer
+        gp = gp and gp.id
+        if gp:
+            itasks = db.query(ITask).filter_by(beneficiary=gp).all()
+            if itasks:
+                testFacility.assertEqual(len(itasks), 1)
+                itask = itasks[0]
+                # mark task as failed
+                itask.state = 'failed'
+                db.updateRecord(itask)
+        else:
+            # or create the itask
+            itask = ITask()
+            itask.id = 'itask-for-job-%s' % job.id
+            itask.beneficiary = job
+            db.insertRow(itask)
+
+        # get the iworker that does the job submission
+        iworker = self.retrieveComponent(
+            'submitjob',
+            factory='iworker',
+            vault = ['iworkers'],
+            )
+        iworker.director = self
+        iworker.run(itask)
+        
+        return
+
+
+    def declareProgress(self, fractional=None, message=None, percentage=None):
+        if fractional is not None and percentage is not None:
+            raise RuntimeError, "both fractional and percentage are supplied"
+        if fractional is not None: percentage = fractional * 100
+        print message, percentage
         return
 
 
