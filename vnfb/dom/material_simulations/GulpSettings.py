40d275cf367a61507ce5b17fdbe3d19ceff81197
J Brandon Keith <jbrkeith@gmail.com>
2010-03-12 22:28:39 +0000

    starting a selenium testing script repository (everything works fine except for login--need to discuss this w/Jiao & company)...further revisions on the md planning guide pop-up
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@2214 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/dom/material_simulations/GulpSettings.py
+++ vnfb/dom/material_simulations/GulpSettings.py
@@ -16,7 +16,7 @@ from _ import o2t
 # still import from vnf-alpha dom. need to change
 #from vnfb.dom.Computable import Computable
 from vnfb.dom.Computation import Computation
-from vnfb.dom.AtomicStructure import StructureTable
+from vnfb.dom.AtomicStructure import Structure
 #from vnfb.dom.MaterialSimulation import MaterialSimulation
 
 #from dsaw.model.Inventory import Inventory as InvBase
@@ -27,10 +27,10 @@ GulpSettings.job_builder = 'material_simulations/gulpSettings'
 GulpSettings.actor = 'material_simulations/gulpSettings'
 GulpSettings.result_retriever = 'material_simulations/gulpSettings'
 
-import dsaw.db
-GulpSettings.addColumn(
-    dsaw.db.reference(name='matter', table=StructureTable, backref='gulpsettings')
-    )
+#import dsaw.db
+#GulpSettings.addColumn(
+#    dsaw.db.reference(name='matter', table=Structure, backref='gulpsettings')
+#    )
 
 
 # version
