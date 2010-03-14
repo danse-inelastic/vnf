de3dae7b71344a6c6446ac3bc575bc9c37ded932
Alexander Dementsov <dexity@gmail.com>
2010-03-14 22:36:47 +0000

    Basic implementation of qe analysis actors
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@2222 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/components/QEAnalysis.py
+++ vnfb/vnfb/components/QEAnalysis.py
@@ -11,6 +11,73 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+import luban.content as lc
+from luban.content import select, load
+from luban.components.AuthorizedActor import AuthorizedActor as base
+
+# Requires simulation id, config id and config type: (id, configid, type)
+class Actor(base):
+
+    class Inventory(base.Inventory):
+        import pyre.inventory
+        id          = pyre.inventory.str('id', default='')          # Simulation Id
+
+
+    def default(self, director):
+        return select(id='main-display-area').replaceContent(self.content(director))
+
+
+    def content(self, director):
+        doc         = lc.document(title="Analysis of Simulation Results")
+        splitter    = doc.splitter(orientation="vertical")
+        sA          = splitter.section()
+        sA.add(self._viewIndicator(director))
+        sB          = splitter.section(id="qe-section-actions")
+        self._showActions(sB)#, sim)               # Show actions
+
+        return doc
+
+
+    def _viewIndicator(self, director):
+        path = []
+        path.append(('Simulations ', load(actor='materialsimulation')))
+        path.append(('Quantum Espresso ', load(actor='materialsimulation')))
+        path.append(('%s ' % self.id, load(actor    = 'material_simulations/espresso/sim-view',
+                                           id       = self.id)))
+
+        path.append('Simulation Results')
+
+        return director.retrieveVisual('view-indicator', path=path)
+
+
+    def _showActions(self, section):#, sim):  #, inputs
+        container   = lc.splitter(orientation="horizontal", id="qe-splitter-analysis")
+        section.add(container)
+        self._backAction(container)
+
+
+    def _backAction(self, container):
+        "Back button"
+        s     = container.section(Class="qe-section-back")
+        s.add(lc.link(label="Back",
+                        Class="qe-action-back",
+                        onclick = load(actor      = 'material_simulations/espresso/sim-view',
+                                         id         = self.id))
+                )
+
+
+    def _configure(self):
+        super(Actor, self)._configure()
+        self.id             = self.inventory.id
+
+
+    def _init(self):
+        super(Actor, self)._init()
+        return
+
+    def __init__(self, name):
+        super(Actor, self).__init__(name=name)
+
 
 __date__ = "$Mar 14, 2010 10:28:08 AM$"
 
