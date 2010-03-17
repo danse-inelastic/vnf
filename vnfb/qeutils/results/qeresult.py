0120f5d3515a7d2e8c9e97c84eb1897ae8444fae
Alexander Dementsov <dexity@gmail.com>
2010-03-17 20:23:43 +0000

    Added stub super class for qe results
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@2262 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ vnfb/qeutils/results/qeresult.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Alex Dementsov
+#                      California Institute of Technology
+#                        (C) 2009  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+class QEResult(object):
+    pass
+
+__date__ = "$Mar 17, 2010 12:21:27 PM$"
+
+
