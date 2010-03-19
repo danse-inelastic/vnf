c746d391e70ffc92ffe570f1a730b097dd1c79b4
Alexander Dementsov <dexity@gmail.com>
2010-03-19 07:09:34 +0000

    Adde stub class for status handling
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@2281 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ vnfb/vnfb/qeutils/status.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Alex Dementsov
+#                      California Institute of Technology
+#                        (C) 2009  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+# For now this class is aiming status display of a job
+
+class Status(object):
+
+    pass
+
+__date__ = "$Mar 18, 2010 11:05:41 PM$"
+
+
