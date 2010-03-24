b60e52aea9e65d7c01fca1e357efc011f4a12160
Alexander Dementsov <dexity@gmail.com>
2010-03-24 21:03:25 +0000

    Updated interface for generators form visuals
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@2320 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/qeutils/generators/phgenerator.py
+++ vnfb/vnfb/qeutils/generators/phgenerator.py
@@ -11,12 +11,79 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+"""
+Generates PH configuration file from scratch
+"""
+from vnfb.qeutils.qeparser.qeinput import QEInput
+from vnfb.qeutils.qeparser.namelist import Namelist
+#from vnfb.qeutils.qeutils import inputRecord, readRecordFile, defaultInputName
+from vnfb.qeutils.results.resultpath import ResultPath
+
+TR2_PH  = "1.0d-16"
+LDISP   = ".true."
+
 class PHGenerator(object):
 
-    def __init__(self, inventory, input):
+    def __init__(self, director, inventory):
+        self._director  = director
         self._inv       = inventory
-        self._input     = input
         self._simtype   = inventory.simtype     # Special case
+
+    # XXX:  Handle prefix properly (should be same as in pw input)
+    def inputph(self):
+        input      = QEInput(type='ph')
+        input.header = "phonon simulation\n"
+        nl  = Namelist("inputph")
+        nl.add("nq1", self._inv.nq1)
+        nl.add("nq2", self._inv.nq2)
+        nl.add("nq3", self._inv.nq3)
+        nl.add("tr2_ph", TR2_PH)
+        nl.add("ldisp", LDISP)
+        #nl.add("prefix", QE_PREFIX)    
+
+        # Add amasses
+        list    = self._amasses()
+        for m in list:
+            nl.add(m[0], m[1])
+
+        input.addNamelist(nl)
+        return input.toString()
+
+
+    def _amasses(self):
+        """Returns list of tuples with amass label and mass value from PW input configuration
+        Example: [("amass(1)", "35.5"), ("amass(2)", "54.3")]
+        """
+        list    = []    # amass list
+        masses  = self._masses()
+        for l in range(len(masses)):
+            list.append(("amass(%s)" % str(l+1), masses[l][1]))
+
+        return list
+
+
+    # XXX: Doesn't handle error when PW input file is not properly written (e.g. "atomic_species" is missing)
+    def _masses(self):
+        # PW configuration input
+        resultpath  = ResultPath(self._director, simid, type)
+        fname       = resultpath.resultFiles("input")
+        try:
+            text    = open(fname).read()    # Try read the file
+        except:
+            return None     # File not read, no masses list returned
+#        pwinput     = inputRecord(director, self.inventory.id, "PW")
+#        fname       = defaultInputName(pwinput.type)
+#        text        = readRecordFile(director.dds, pwinput, fname)
+        pw          = QEInput(config = text)
+        pw.parse()
+
+        list        = []
+        # List of atom of format: [('Ni', '52.98', 'Ni.pbe-nd-rrkjus.UPF'), (...)]
+        atoms       = pw.structure()
+        for a in atoms:
+            list.append((a[0], a[1]))
+
+        return list
 
 
 __date__ = "$Mar 24, 2010 9:59:39 AM$"
