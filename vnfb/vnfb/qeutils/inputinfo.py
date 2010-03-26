f1d3ce08e719f3e5bcbeb5e7b2e24dba5226629a
Alexander Dementsov <dexity@gmail.com>
2010-03-26 19:47:47 +0000

    Set "Add" link in InputInfo class to a separate method. We are growing :)
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@2346 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/qeutils/inputinfo.py
+++ vnfb/vnfb/qeutils/inputinfo.py
@@ -11,6 +11,8 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+from vnfb.qeutils.qeutils import latestInput
+
 from luban.content import load
 from luban.content.Link import Link
 
@@ -24,31 +26,40 @@ class InputInfo:
         self._taskid    = taskid
         self._type      = type
 
+
     def getLink(self):      # simulation
         inputs  = self._director.clerk.getQEConfigurations(where="taskid='%s'" % self._taskid )
+
+        link    = self._linkAdd()
+        if not inputs:      # No inputs created, return "Add" link
+            return link
+
+        input   = latestInput(inputs)
+        if not input:       # No input, return "Add" link
+            return link
+
+        return Link(label   = input.filename,
+                    onclick = load(actor      = "material_simulations/espresso/input-view",
+                                 id         = self._id,
+                                 taskid     = self._taskid,
+                                 type       = self._type,
+                                 configid   = input.id)   # Some more?
+                    )
+
+
+    # XXX: Make it more flexible
+    def _linkAdd(self):
+        "Returns link 'Add'"
         sim     = self._director.clerk.getQESimulations(id = self._id)
 
-        link = Link(label="Add", 
+        link = Link(label="Add",
                     onclick=load(actor      = "material_simulations/espresso/input-generate",
                                  id         = self._id,
                                  taskid     = self._taskid,
                                  type       = self._type,
                                  structureid    = sim.structureid)
                     )
-
-        if inputs:
-            s = inputs[0]
-            if s:
-                link = Link(label=s.filename,
-                            onclick=load(actor      = "material_simulations/espresso/input-view",
-                                         id         = self._id,
-                                         taskid     = self._taskid,
-                                         type       = self._type,
-                                         configid   = s.id)   # Some more?
-                            )
-
         return link
-
 
 if __name__ == "__main__":
     pass
