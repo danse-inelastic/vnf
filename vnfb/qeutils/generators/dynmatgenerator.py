61b68372b40f5b01029aa422a9ceca8f42f8e6ba
Alexander Dementsov <dexity@gmail.com>
2010-03-28 18:06:07 +0000

    Implemented dynmat generator
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@2357 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/qeutils/generators/dynmatgenerator.py
+++ vnfb/qeutils/generators/dynmatgenerator.py
@@ -11,20 +11,47 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+from vnfb.qeutils.results.phresult import PHResult
 from vnfb.qeutils.qeparser.qeinput import QEInput
 from vnfb.qeutils.qeparser.namelist import Namelist
+from vnfb.qeutils.qeconst import ZASR, ZASRLIST
 
 class DYNMATGenerator(object):
 
-    def __init__(self, inventory, input):
+    def __init__(self, director, inventory):
+        self._director  = director
         self._inv       = inventory
-        self._input     = input
+        self._input     = None
+        self._phresults = PHResult(self._director, self._inv.id)
 
 
+    # XXX: Force parameters: "fildyn", "filout"
     def setInput(self):
+        "Set namelist 'input'"
         self._input      = QEInput(type='dynmat')
         nl  = Namelist("input")
         self._input.addNamelist(nl)
+
+        nl.add("fildyn",    self._phresults.fildyn()) # from PH results
+        self._addAsr(nl)
+        self._addQPoint(nl)
+
+
+    def _addAsr(self, nl):
+        "Adds 'asr' parameter for gamma point only"
+        if self._phresults.isGammaPoint():
+            asr         = ZASR[ZASRLIST[int(self._inv.asr)]]
+            nl.add("asr",      asr)
+
+
+    def _addQPoint(self, nl):
+        "Adds q point"
+        kp          = self._phresults.kCoord()
+        if not kp:
+            nl.add("q(#)", "ERROR: Phonon coordinate (Kx, Ky, Kz) are not set on PH input")
+
+        for i in range(len(kp)):
+            nl.add("q(%s)" % str(i+1), kp[i])
 
 
     def toString(self):
