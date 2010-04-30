5cea0498db48bd964e65d640dca1f7fa6d72bce4
Jiao Lin <linjiao@caltech.edu>
2010-04-30 23:38:51 +0000

    vnfb/tests/selenium/login_TestCase.py: added the first unittest testcase using selenium
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@2620 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ vnfb/tests/selenium/login_TestCase.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                      (C) 2006-2010 All Rights Reserved 
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+from luban.testing.selenium.TestCaseBase import TestCaseBase as base, makePySuite
+
+class TestCaseBase(base):
+
+    targetapp = 'vnf'
+
+
+    def test1(self):
+        'vnf: login'
+        s = self.selenium
+
+        s.open(self.appaddress)
+
+        username = "//div[@id='login-username-input']"
+        password = "//div[@id='login-password-input']"
+        inputsubelem = '/table/tbody/tr/td[1]/input'
+        s.waitForElementPresent(username)
+        s.type(username+inputsubelem, 'demo')
+        s.type(password+inputsubelem, 'demo')
+
+        submit = "//input[@type='submit']"
+        s.click(submit)
+
+        minimize = "//div[@id='minimize-help-button']/a/span"
+        s.waitForElementPresent(minimize)
+        s.click(minimize)
+
+        return
+
+
+def pysuite():
+    from vnfb.testing import getDeploymentInfo
+    info = getDeploymentInfo()
+    fixtures = info.selenium_test_fixtures
+    return makePySuite(TestCaseBase, fixtures)
+
+
+def main():
+    pytests = pysuite()
+    import unittest
+    alltests = unittest.TestSuite( (pytests, ) )
+    unittest.TextTestRunner(verbosity=2).run(alltests)
+    
+    
+if __name__ == "__main__": main()
+    
+# version
+__id__ = "$Id$"
+
+# End of file 
