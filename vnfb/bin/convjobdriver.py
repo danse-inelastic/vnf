15c2d93c6baed982e42577242ffdf099c6b1bc32
Alexander Dementsov <dexity@gmail.com>
2010-05-03 05:05:07 +0000

    First row in convergence job table now looks as it should :)
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@2652 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/bin/convjobdriver.py
+++ vnfb/bin/convjobdriver.py
@@ -11,6 +11,21 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+def main():
+    from vnfb.applications.ConvJobDriver import ConvJobDriver
+
+    class App(ConvJobDriver):
+
+        def _getPrivateDepositoryLocations(self):
+            return ['/tmp/luban-services', '../config', '../content/components']
+
+    app = App()
+    return app.run()
+
+# main
+if __name__ == '__main__':
+    # invoke the application shell
+    main()
 
 __date__ = "$May 2, 2010 3:51:09 PM$"
 
