269b2b53a0e49f2176d3b79075e26be837b1a0d1
Jiao Lin <linjiao@caltech.edu>
2010-05-08 15:32:17 +0000

    docs/Make.mm: make use of sphinx rules in config
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@2685 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/docs/Make.mm
+++ vnfb/docs/Make.mm
@@ -14,22 +14,10 @@ PROJECT = vnf
 PACKAGE = sphinx
 
 
-RECURSE_DIRS = \
-
-
-# data dirs containing images, videos etc that will be exported to 
-# sphinx export directory
 EXPORT_DATADIRS = \
-	_static \
 
 
-# vars for sphinx build command 
-# build cmd for sphinx
-SPHINX_BUILD   = sphinx-build
-# additional options for sphinx command
-SPHINX_OPTS    =
-# paper type: a4 or letter
-PAPER         =                         
+RECURSE_DIRS = \
 
 
 #--------------------------------------------------------------------------
@@ -37,9 +25,6 @@ PAPER         =
 
 all: docs
 	BLD_ACTION="all" $(MM) recurse
-
-PROJ_CLEAN = \
-	_build/* \
 
 clean::
 	BLD_ACTION="clean" $(MM) recurse
@@ -50,39 +35,11 @@ distclean::
 tidy::
 	BLD_ACTION="tidy" $(MM) recurse
 
-docs: export-data sphinx-build 
+docs: sphinx-build export-sphinx-data
 
 
 include std-docs.def
-
-RSYNC_A = rsync -a
-
-# tmp dir for this package
-PACKAGE_TMPDIR = $(BLD_TMPDIR)/$(PROJECT)/$(PACKAGE)
-
-
-# to build up the sphinx cmd
-SPHINX_BUILDDIR = $(EXPORT_DOCDIR)
-SPHINX_BUILDDIR_HTML = $(SPHINX_BUILDDIR)
-SPHINX_BUILDDIR_DOCTREES = $(PACKAGE_TMPDIR)/doctrees
-PAPEROPT_a4     = -D latex_paper_size=a4
-PAPEROPT_letter = -D latex_paper_size=letter
-ALL_SPHINX_OPTS   = -d $(SPHINX_BUILDDIR_DOCTREES) $(PAPEROPT_$(PAPER)) $(SPHINX_OPTS) .
-
-
-sphinx-build: $(EXPORT_DOCDIR)
-	mkdir -p $(SPHINX_BUILDDIR_HTML)
-	mkdir -p $(SPHINX_BUILDDIR_DOCTREES)
-	$(SPHINX_BUILD) -b html $(ALL_SPHINX_OPTS) $(SPHINX_BUILDDIR_HTML)
-	@echo
-	@echo "Build finished. The HTML pages are in $(SPHINX_BUILDDIR_HTML)."
-
-
-export-data: $(EXPORT_DATADIRS) $(SPHINX_BUILDDIR_HTML)
-	for x in $(EXPORT_DATADIRS); do { \
-	  $(RSYNC_A) $$x/ $(SPHINX_BUILDDIR_HTML)/$$x/; \
-	} done
-
+include sphinx/default.def
 
 # version
 # $Id: Make.mm,v 1.2 2008-04-13 03:55:58 aivazis Exp $
