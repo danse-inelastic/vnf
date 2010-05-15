c650c412805eb69bda13b5d152e49336e35a0368
Jiao Lin <linjiao@caltech.edu>
2010-05-15 16:27:49 +0000

    material_simulations/bvkutils_TestCase.py: added a test case
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@2744 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- tests/vnfb/utils/material_simulations/bvkutils_TestCase.py
+++ tests/vnfb/utils/material_simulations/bvkutils_TestCase.py
@@ -95,6 +95,33 @@ class TestCase(unittest.TestCase):
         return
 
 
+    def test3a(self):
+        # simple cubic
+        import matter
+        lattice = matter.Lattice(a=3.701, b=3.701, c=3.701, alpha=90, beta=90, gamma=90)
+        atom1 = matter.Atom('C')
+        atom2 = matter.Atom('H', [0.5, 0.5, 0])
+        atom3 = matter.Atom('H', [0.5, 0, 0.5])
+        atom4 = matter.Atom('H', [0, 0.5, 0.5])
+        struct = matter.Structure([atom1, atom2, atom3, atom4], lattice=lattice, sgid=221)
+
+        from bvk.BvKBond import BvKBond
+        bond = BvKBond()
+        bond.matter = struct
+        bond.uses_primitive_unitcell = 1
+        bond.A = 0
+        bond.B = 1
+        bond.Boffset = [0,0.5,0.5]
+        bond.Boffset_is_fractional = 1
+        
+        # 110
+        print 'bond 00.50.5 for sc lattice'
+        for constraint in  findForceContantTensorConstraints(bond):
+            print constraint
+            
+        return
+
+
 def main():
     unittest.main()
     return
