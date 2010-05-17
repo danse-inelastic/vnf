c39a85719055f3483af22b9e2c5b3a56e5c70b41
Jiao Lin <linjiao@caltech.edu>
2010-05-17 15:34:42 +0000

    dom.neutron_instrument_simulations.neutron_components.ChanneledGuide: auto-generated from Channeled_guide
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@2775 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/dom/neutron_experiment_simulations/neutron_components/create-components.sh
+++ vnfb/dom/neutron_experiment_simulations/neutron_components/create-components.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 ./mcstas-component-to-dom.py  -type=IQE_monitor -category=monitors --base=Monitor --tablebase=MonitorTableBase --newname=QEMonitor --skip-props=filename
+./mcstas-component-to-dom.py  -type=Channeled_guide -category=obsolete --newname=ChanneledGuide
