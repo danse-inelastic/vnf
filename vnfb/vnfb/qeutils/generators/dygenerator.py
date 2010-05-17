f28bd2c6baa9c1b8ab24d94b1fea3c0ecffde991
Alexander Dementsov <dexity@gmail.com>
2010-05-17 17:42:15 +0000

    Partially implemented generator for dynamics task
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@2783 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/qeutils/generators/dygenerator.py
+++ vnfb/vnfb/qeutils/generators/dygenerator.py
@@ -11,6 +11,13 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+class DYGenerator(object):
+    "Generator for CP Dynamics task"
+
+    def __init__(self, director, inventory, input = None):
+        self._director  = director
+        self._inv       = inventory
+        self._input     = input
 
 __date__ = "$May 16, 2010 10:02:45 AM$"
 
