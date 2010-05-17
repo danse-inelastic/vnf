c8fc0a9f64db372291d7dcc0f554387875c3595e
Alexander Dementsov <dexity@gmail.com>
2010-05-17 18:14:17 +0000

    Partially implemented generator for trajectory task
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@2786 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/qeutils/generators/trgenerator.py
+++ vnfb/vnfb/qeutils/generators/trgenerator.py
@@ -11,7 +11,14 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+class TRGenerator(object):
+    "Generator for CP Trajectory Analysis task"
 
+    def __init__(self, director, inventory, input = None):
+        self._director  = director
+        self._inv       = inventory
+        self._input     = input
+        
 __date__ = "$May 16, 2010 10:04:49 AM$"
 
 
