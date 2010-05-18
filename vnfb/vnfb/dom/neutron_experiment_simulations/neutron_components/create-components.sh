e44629a3505e5de2653c9c2794c3a7cd2e791dfd
Jiao Lin <linjiao@caltech.edu>
2010-05-18 18:37:52 +0000

    vnfb/dom/neutron_experiment_simulations/neutron_components/create-components.sh: minor
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@2818 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/dom/neutron_experiment_simulations/neutron_components/create-components.sh
+++ vnfb/vnfb/dom/neutron_experiment_simulations/neutron_components/create-components.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ./mcstas-component-to-dom.py  -type=IQE_monitor -category=monitors --base=Monitor --tablebase=MonitorTableBase --newname=QEMonitor --skip-props=filename
-./mcstas-component-to-dom.py  -type=E_monitor -category=monitors --base=Monitor --tablebase=MonitorTableBase --newname=EMonitor --skip-props=filename
+#./mcstas-component-to-dom.py  -type=E_monitor -category=monitors --base=Monitor --tablebase=MonitorTableBase --newname=EMonitor --skip-props=filename
 #./mcstas-component-to-dom.py  -type=TOF_monitor2 -category=monitors --base=Monitor --tablebase=MonitorTableBase --newname=TofMonitor --skip-props=filename
 #./mcstas-component-to-dom.py  -type=Q_monitor -category=monitors --base=Monitor --tablebase=MonitorTableBase --newname=QMonitor --skip-props=filename
 #./mcstas-component-to-dom.py  -type=PSD_monitor_4PI -category=monitors --base=Monitor --tablebase=MonitorTableBase --newname=SphericalPSD --skip-props=filename
