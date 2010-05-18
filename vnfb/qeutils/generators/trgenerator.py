55d37372cb718a3ecea48c526473e9b192e43b32
Alexander Dementsov <dexity@gmail.com>
2010-05-18 11:34:06 +0000

    Implemented generator for the trajectory task type
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@2806 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/qeutils/generators/trgenerator.py
+++ vnfb/qeutils/generators/trgenerator.py
@@ -11,6 +11,9 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+import StringIO
+import ConfigParser
+
 class TRGenerator(object):
     "Generator for CP Trajectory Analysis task"
 
@@ -19,9 +22,42 @@ class TRGenerator(object):
         self._inv       = inventory
         self._input     = input
 
+        self._init()
+
+
+    def _init(self):
+        if not self._input:
+            self._fp    = StringIO.StringIO()
+            self._input = ConfigParser.ConfigParser()
+
+
+    def setSystem(self):
+        "Sets system parameters"
+        self._input.add_section("system")
+        self._input.set("system", "start", self._inv.start)
+        self._input.set("system", "end", self._inv.end)
+
+        
+    def setAnalysis(self):
+        "Sets analysis specific parameters"
+        self._input.add_section("analysis")
+        self._input.set("analysis", "vdos", self._inv.vdos)
+        self._input.set("analysis", "vdist", self._inv.vdist)
+        self._input.set("analysis", "msd", self._inv.msd)
+        self._input.set("analysis", "rdf", self._inv.rdf)
+        self._input.set("analysis", "temptime", self._inv.temptime)
+
 
     def toString(self):
-        return "Hi"
+        if not self._input:
+            return "TRGenerator"
+
+        self._input.write(self._fp)
+        str = self._fp.getvalue()
+        self._fp.close()
+        return str
+
+        
 
 __date__ = "$May 16, 2010 10:04:49 AM$"
 
