b88d91d051d7ca2c7ab2c97c2a490171e8895156
Alexander Dementsov <dexity@gmail.com>
2010-05-18 11:53:17 +0000

    Updated parameters in the trajectory generator
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@2807 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/qeutils/generators/trgenerator.py
+++ vnfb/vnfb/qeutils/generators/trgenerator.py
@@ -14,6 +14,11 @@
 import StringIO
 import ConfigParser
 
+VELFILE     = "cp.vel"
+POSFILE     = "cp.pos"
+NCFILE      = "cp.nc"
+DOSFILE     = "vdos.dos"
+
 class TRGenerator(object):
     "Generator for CP Trajectory Analysis task"
 
@@ -34,18 +39,27 @@ class TRGenerator(object):
     def setSystem(self):
         "Sets system parameters"
         self._input.add_section("system")
-        self._input.set("system", "start", self._inv.start)
-        self._input.set("system", "end", self._inv.end)
+        self._input.set("system", "fvel",   VELFILE)    # Velocities file
+        self._input.set("system", "fpos",   POSFILE)    # Position file
+        self._input.set("system", "fnc",    NCFILE)     # Trajectory file in netCDF format
+        self._input.set("system", "fdos",   DOSFILE)    # Dos file
 
-        
+
+    def setSteps(self):
+        "Sets system parameters"
+        self._input.add_section("steps")
+        self._input.set("steps", "start", self._inv.start)
+        self._input.set("steps", "end", self._inv.end)
+
+
     def setAnalysis(self):
         "Sets analysis specific parameters"
         self._input.add_section("analysis")
-        self._input.set("analysis", "vdos", self._inv.vdos)
-        self._input.set("analysis", "vdist", self._inv.vdist)
-        self._input.set("analysis", "msd", self._inv.msd)
-        self._input.set("analysis", "rdf", self._inv.rdf)
-        self._input.set("analysis", "temptime", self._inv.temptime)
+        self._input.set("analysis", "vdos", self._inv.vdos)     # Vibrational Density of States (VDOS)
+        self._input.set("analysis", "vdist", self._inv.vdist)   # Velocity Distribution
+        self._input.set("analysis", "msd", self._inv.msd)       # Mean-Square Displacement
+        self._input.set("analysis", "rdf", self._inv.rdf)       # Radial Distribution Function
+        self._input.set("analysis", "temptime", self._inv.temptime) # Temperature Dependence on Time
 
 
     def toString(self):
