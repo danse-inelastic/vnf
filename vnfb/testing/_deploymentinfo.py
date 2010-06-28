31262ec8aea2a8a49e4610b87e75c1f9230a7622
Jiao Lin <linjiao@caltech.edu>
2010-06-28 02:38:32 +0000

    added "deployment" python module. was moved from testing._deploymentinfo
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@2899 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/testing/_deploymentinfo.py
+++ vnfb/testing/_deploymentinfo.py
@@ -12,28 +12,7 @@
 #
 
 
-dbname = 'postgres:///vnfbeta'
-
-import os
-exportroot = os.environ.get('EXPORT_ROOT') or os.environ.get('PYRE_DIR')
-if not exportroot:
-    raise RuntimeError, "cannot figure out export root directory"
-
-
-vnfbexportroot = os.path.join(exportroot, 'vnfb')
-contentroot = os.path.join(vnfbexportroot, 'content')
-componentsroot = os.path.join(contentroot, 'components')
-dataroot = os.path.join(contentroot, 'data')
-configdir = os.path.join(vnfbexportroot, 'config')
-guid_datastore_path = os.path.join(configdir, 'guid.dat')
-
-lubanserviceshome = '/tmp/luban-services'
-
-pyre_depositories = [
-    configdir,
-    componentsroot,
-    lubanserviceshome,
-    ]
+from vnfb.deployment import *
 
 
 # version
