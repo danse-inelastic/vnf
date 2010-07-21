8a9782aaa85d1a88415e673ec98f0ce7679f3bc4
Alexander Dementsov <dexity@gmail.com>
2010-07-21 02:40:01 +0000

    Implemented ion-min generator
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@2939 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/qeutils/generators/imgenerator.py
+++ vnfb/qeutils/generators/imgenerator.py
@@ -16,6 +16,12 @@ IMGenerator - generates Ion Minimization configuration input based on
     Electron Minization configuration input
 """
 
+NDW                 = "52"
+ELECTRON_DYNAMICS   = "'damp'"
+ELECTRON_DAMPING    = "0.2"
+ION_DYNAMICS        = "'damp'"
+ION_DAMPING         = "0.02"
+
 from vnfb.qeutils.generators.cpgenerator import CPGenerator as base
 
 class IMGenerator(base):
@@ -28,9 +34,33 @@ class IMGenerator(base):
         control  = self._input.namelist("control")
         control.set("dt", self._inv.dt)
         control.set("nstep", self._inv.nstep)
-        control.set("restart_mode", "'restart'")
-        
-        
+        self._setNdw(control)
+        #control.set("restart_mode", "'restart'")   # Should restart instead?
+
+
+    def setElectrons(self):
+        electrons  = self._input.namelist("electrons")
+        electrons.set("electron_dynamics", ELECTRON_DYNAMICS)
+        electrons.set("electron_damping", ELECTRON_DAMPING)
+
+
+    def setIons(self):
+        ions        = self._input.namelist("ions")
+        ions.set("ion_dynamics", ION_DYNAMICS)
+        ions.set("ion_damping", ION_DAMPING)
+
+
+    def _setNdw(self, control):
+        "Sets 'ndw' parameter"
+        if not control:
+            return
+
+        ndw         = control.param("ndw")
+        if ndw:
+            control.set("ndw", str(int(ndw)+1))
+            return
+
+        control.set("ndw", NDW)
 
 
 __date__ = "$May 16, 2010 10:01:45 AM$"
