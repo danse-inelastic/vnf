ecbaf70ef59d040eeebc9cae97d00cff7ff46e05
Jiao Lin <linjiao@caltech.edu>
2010-07-30 01:37:57 +0000

    tests: components/Clerk_TestCase.py: added a test case for storing a scatterer
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@2985 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- tests/vnfb/components/Clerk_TestCase.py
+++ tests/vnfb/components/Clerk_TestCase.py
@@ -12,31 +12,94 @@
 #
 
 
-import unittest
-class TestCase(unittest.TestCase):
 
-    def test1(self):
-        from vnfb.components.Clerk import Clerk
-        clerk = Clerk()
-        from vnfb.deployment import dbname
-        clerk.inventory.db = dbname
+# application
+from luban.applications.UIApp import UIApp as base
+class TestApp(base):
+
+
+    class Inventory(base.Inventory):
+
+        import pyre.inventory
+
+
+    def main(self, testFacility, *args, **kwds):
+        self.test1(testFacility)
+        self.test2(testFacility)
+        return
+
+
+    def test2(self, tf):
+        clerk = self.clerk
+        
+        # clerk.importAllDataObjects()
+        db = clerk.db
+        orm = clerk.orm
+
+        # load
+        # from vnfb.dom.Atom import Atom
+        # atom = orm.load(Atom, '3BEYWG5W')
+
+        # save matter
+        from vnfb.dom.AtomicStructure import Structure
+        matter = Structure()
+        matter.short_description = 'test structure'
+        orm.save(matter)
+
+        # save scatterer
+        from vnfb.dom.neutron_experiment_simulations.Scatterer import Scatterer
+        scatterer = Scatterer()
+        scatterer.short_description = 'test'
+        scatterer.matter = matter
+        orm = clerk.orm
+        orm.save(scatterer, save_not_owned_referred_object=False)
+        return
+    
+
+    def test1(self, tf):
+        clerk = self.clerk
         clerk.importAllDataObjects()
         db = clerk.db
         tablenames = [t.getTableName() for t in db._tableregistry.itertables()]
 
         # check some tables
         # bvk
-        self.assert_('bvkbonds' in tablenames)
+        tf.assert_('bvkbonds' in tablenames)
 
         # atomic structure
         structuretable = db.getTable('atomicstructures')
         from vnfb.dom.AtomicStructure import StructureTable, AbstractOwnedObjectBase
-        self.assertEqual(StructureTable, structuretable)
+        tf.assertEqual(StructureTable, structuretable)
         AbstractOwnedObjectBase.creator
         return
 
-    
 
+    def _defaults(self):
+        super(TestApp, self)._defaults()
+        from vnfb.components.AppExtension import AppExtension
+        self.inventory.extension = AppExtension('app-extension')
+        from vnfb.components.Clerk import Clerk
+        self.inventory.clerk = Clerk()
+        return
+
+
+    def _getPrivateDepositoryLocations(self):
+        from vnfb import deployment
+        print deployment.pyre_depositories
+        return deployment.pyre_depositories
+
+
+
+
+import unittest
+class TestCase(unittest.TestCase):
+
+    def test(self):
+        app = TestApp('test')
+        app.run(self)
+        return
+    
+        
 def main():
     unittest.main()
     return
