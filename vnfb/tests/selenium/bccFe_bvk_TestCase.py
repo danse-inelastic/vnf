758adf159a7dbe0d83df29994c499c13c431548a
Jiao Lin <linjiao@caltech.edu>
2010-08-05 19:11:05 +0000

    tests: selenium/bccFe_bvk_TestCase.py: started
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@3005 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ vnfb/tests/selenium/bccFe_bvk_TestCase.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                      (C) 2006-2010 All Rights Reserved 
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+from luban.testing.selenium.TestCaseBase import TestCaseBase as base, makePySuite
+
+class TestCaseBase(base):
+
+    targetapp = 'vnf'
+
+
+    def test1(self):
+        'vnf: bvk for bcc Fe (demo user)'
+        s = self.selenium
+        lh = s.lh
+        
+        s.open(self.appaddress)
+
+        from workflows.basic import login, basic_filter
+        login(s)
+
+        basic_filter(s, table='atomicstructure', key='description', value='bcc Fe*')
+
+        table = lh.selector('table', id='atomicstructure-table')
+        structlink = table + '/tbody/tr[1]/td[2]/a'
+        s.waitForElementPresent(structlink)
+        s.click(structlink)
+
+        lh.expandDocument(id = 'atomicstructure-computed-phonons')
+
+        startnew_link = lh.selector('a', id='start-new-phonon-computation-link')
+        s.waitForElementPresent(startnew_link)
+        s.click(startnew_link)
+
+        lh.sleep(4)
+        self.assert_(s.get_alert())
+        return
+        
+
+    def test2(self):
+        'vnf: bvk for bcc Fe (real user)'
+        from realuser import username, password
+        
+        s = self.selenium
+        lh = s.lh
+        
+        s.open(self.appaddress)
+
+        from workflows.basic import login, basic_filter
+        login(s, username=username, password=password)
+
+        basic_filter(s, table='atomicstructure', key='description', value='bcc Fe*')
+
+        table = lh.selector('table', id='atomicstructure-table')
+        structlink = table + '/tbody/tr[1]/td[2]/a'
+        s.waitForElementPresent(structlink)
+        s.click(structlink)
+
+        lh.expandDocument(id = 'atomicstructure-computed-phonons')
+
+        startnew_link = lh.selector('a', id='start-new-phonon-computation-link')
+        s.waitForElementPresent(startnew_link)
+        s.click(startnew_link)
+
+        lh.sleep(5)
+        return
+        
+
+def pysuite():
+    from vnfb.testing import getDeploymentInfo
+    info = getDeploymentInfo()
+    fixtures = info.selenium_test_fixtures
+    return makePySuite(TestCaseBase, fixtures)
+
+
+def main():
+    pytests = pysuite()
+    import unittest
+    alltests = unittest.TestSuite( (pytests, ) )
+    unittest.TextTestRunner(verbosity=2).run(alltests)
+    
+    
+if __name__ == "__main__": main()
+    
+# version
+__id__ = "$Id$"
+
+# End of file 
