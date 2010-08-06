5e2a3a7ad15a7001c0c6a262789c4386411881ce
Alexander Dementsov <dexity@gmail.com>
2010-08-06 20:25:23 +0000

    Implemented base class for filters
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@3017 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ vnfb/vnfb/qeutils/filters/filter.py
@@ -0,0 +1,49 @@
+# -*- Python -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Alex Dementsov
+#                      California Institute of Technology
+#                        (C) 2010  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+from vnfb.qeutils.qeparser.filter import Filter as InputFilter
+
+"""
+Filter - base class for all filters
+"""
+
+class Filter(object):
+
+    def __init__(self):
+        self._plus      = InputFilter("fplus")
+        self._minus     = InputFilter("fminus")
+        self._setPlusFilter()
+        self._setMinusFilter()
+
+
+    def _setPlusFilter(self):
+        print "WARNING: This class is not for direct use. Please subclass"
+
+
+    def _setMinusFilter(self):
+        print "WARNING: This class is not for direct use. Please subclass"
+
+
+    def apply(self, input):
+        """
+        Apply filter to input object
+
+            input: (object: QEInput) 
+        """
+        self._plus.apply(input, "plus")
+        self._minus.apply(input, "minus")
+
+
+__date__ = "$Aug 6, 2010 12:31:10 PM$"
+
+
