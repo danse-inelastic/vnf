744ce80ae8016e0c83b0da43e90417b149996eba
Alexander Dementsov <dexity@gmail.com>
2010-08-18 19:03:11 +0000

    Implemented filters for bands, dos and cp
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@3056 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/qeutils/filters/cpfilter.py
+++ vnfb/vnfb/qeutils/filters/cpfilter.py
@@ -17,13 +17,17 @@ from filter import Filter
 CPFilter - configuration input filter for cp task type.
 """
 
-class CPFilter(Filter):
-    pass
+PREFIX  = "'cp'"
 
-#    def _setMinusFilter(self):
-#        self._minus.setParam("control", "prefix")        # Will be set to default ('pwscf')
-#        self._minus.setParam("control", "pseudo_dir")   # Will be set to $ESPRESSO_PSEUDO
-#        self._minus.setParam("control", "outdir")       # Will be set to $ESPRESSO_TMPDIR
+class CPFilter(Filter):
+
+    def _setPlusFilter(self):
+        self._plus.setParam("control", "prefix", PREFIX)
+
+
+    def _setMinusFilter(self):
+        self._minus.setParam("control", "pseudo_dir")   # Will be set to $ESPRESSO_PSEUDO
+        self._minus.setParam("control", "outdir")       # Will be set to $ESPRESSO_TMPDIR
 
 
 __date__ = "$Aug 6, 2010 12:16:13 PM$"
--- vnfb/vnfb/qeutils/filters/dosfilter.py
+++ vnfb/vnfb/qeutils/filters/dosfilter.py
@@ -17,13 +17,18 @@ from filter import Filter
 DOSFilter - configuration input filter for dos task type.
 """
 
-class DOSFilter(Filter):
-    pass
+PREFIX  = "'pwscf'"
+FILDOS  = "pwscf.dos"   # Set apostrophes?
 
-#    def _setMinusFilter(self):
-#        self._minus.setParam("control", "prefix")        # Will be set to default ('pwscf')
-#        self._minus.setParam("control", "pseudo_dir")   # Will be set to $ESPRESSO_PSEUDO
-#        self._minus.setParam("control", "outdir")       # Will be set to $ESPRESSO_TMPDIR
+class DOSFilter(Filter):
+    
+    def _setPlusFilter(self):
+        self._plus.setParam("inputpp", "prefix", PREFIX)
+        self._plus.setParam("inputpp", "filband", FILDOS)
+
+
+    def _setMinusFilter(self):
+        self._minus.setParam("inputpp", "outdir")       # Will be set to $ESPRESSO_TMPDIR
 
 
 __date__ = "$Aug 6, 2010 12:16:13 PM$"
