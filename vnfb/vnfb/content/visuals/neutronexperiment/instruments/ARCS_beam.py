db5765cb462641ed1377a2001969f840c7779cbd
Jiao Lin <linjiao@caltech.edu>
2010-08-19 07:55:52 +0000

    configuration wizard for ARCS instrument mostly working
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@3069 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/content/visuals/neutronexperiment/instruments/ARCS_beam.py
+++ vnfb/vnfb/content/visuals/neutronexperiment/instruments/ARCS_beam.py
@@ -21,17 +21,26 @@ def configuration_wizard(experiment):
         id='instrument-configuration-wizard',
         )
 
-    load_manual_configuration = load(
-        actor='loadvisual',
-        visual='neutronexperiment/edit/instrumentpanel',
-        mode = 'manualconfiguration',
-        id=experiment.id
-        )
+    # button to skip this wizard
+    from . import load_manual_instrument_configuration
+    load_manual_configuration = load_manual_instrument_configuration(experiment.id)
     skip_button = lc.button(
-        label='skip',
+        label='skip this wizard',
         onclick=select(element=doc).replaceBy(load_manual_configuration),
         )
     doc.add(skip_button)
+
+    # wizard
+    form = doc.form(id='arcs-beam-wizard')
+    form.text(name='fermi_nu', label = 'Fermi chopper frequency (Hz)', value=600)
+    # form.text(name='fermi_bladeradius', label = 'Fermi choipper blade radius (meter)', value=0.060364)
+    form.text(name='T0_nu', label = 'T0 chopper frequency (Hz)', value=120)
+    form.text(name='E', label = 'Desired incident energy (meV)', value=100)
+    form.text(name='emission_time', label = 'Emission time (microsecond)', value=10)
+    form.submitbutton()
+    form.onsubmit = select(element=form).submit(
+        actor='neutronexperiment/edit/ARCS_beam_wizard',
+        experiment_id = experiment.id)
     
     return doc
 
--- vnfb/vnfb/content/visuals/neutronexperiment/instruments/__init__.py
+++ vnfb/vnfb/content/visuals/neutronexperiment/instruments/__init__.py
@@ -11,6 +11,17 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+
+def load_manual_instrument_configuration(experiment_id):
+    from luban.content import load
+    return load(
+        actor='loadvisual',
+        visual='neutronexperiment/edit/instrumentpanel',
+        mode = 'manualconfiguration',
+        id=experiment_id
+        )
+
+
 # version
 __id__ = "$Id$"
 
