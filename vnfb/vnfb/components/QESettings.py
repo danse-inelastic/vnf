f4bffbfab1d9d5c07f30e295afdfdb8f82b92db3
Alexander Dementsov <dexity@gmail.com>
2010-08-26 02:15:26 +0000

    Got rid of unnecessary parameter sim in QESettings
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@3086 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/components/QESettings.py
+++ vnfb/vnfb/components/QESettings.py
@@ -13,10 +13,11 @@
 
 from vnfb.dom.QESetting import QESetting
 from vnfb.qeutils.qegrid import QEGrid
-
+from luban.content.FormSelectorField import FormSelectorField
 from luban.content import select
 
 from vnfb.qeutils.qeconst import SETTINGS, PROCESSORS, NOPARALSIM, SIMTYPE, OPT_DEFAULT
+from vnfb.qeutils.qeconst import ID_SELECTOR_CORES
 from vnfb.qeutils.qeutils import serverName
 from vnfb.utils.serverlist import ServerList
 
@@ -27,7 +28,8 @@ class Actor(base):
 
     class Inventory(base.Inventory):
         import pyre.inventory
-        id          = pyre.inventory.str('id', default='')          # Simulation Id
+        id          = pyre.inventory.str('id', default='')
+        type        = pyre.inventory.str('type', default='')
         server      = pyre.inventory.str('server', default='')
         sname       = pyre.inventory.str('sname', default='')
         description = pyre.inventory.str('description', default='') # Not used
@@ -41,7 +43,15 @@ class Actor(base):
 
     def setCoresList(self, director):
         "Sets cores list depending on server selection"
-        print self.server
+        procfield   = "Hello"
+        return select(id=ID_SELECTOR_CORES).replaceContent(procfield)
+
+
+    def _selectorCores(self, servlist):
+        cores           = lc.document(id=ID_SELECTOR_CORES)
+        procfield       = FormSelectorField(name="numproc", entries = self._procOptions(servlist, 0))
+        cores.add(procfield)
+        return cores
 
 
     def _optLevel(self):
@@ -56,11 +66,9 @@ class Actor(base):
         return 0
 
 
-    def _procOptions(self, sim, servlist, servorder):
+    def _procOptions(self, servlist, servorder):
         "Available options for number of cores"
         DEFAULT     = enumerate((1,))
-        if not sim:
-            return DEFAULT
 
         # servorder is out of range
         if not servorder in range(len(servlist)):
@@ -71,7 +79,7 @@ class Actor(base):
         DEFAULT     = enumerate(PROCESSORS[shortname])
 
         for k in SIMTYPE.keys():
-            if SIMTYPE[k] == sim.type and k in NOPARALSIM:
+            if SIMTYPE[k] == self.type and k in NOPARALSIM:
                 return enumerate((1,))  # Single core
 
         return DEFAULT
@@ -94,6 +102,7 @@ class Actor(base):
     def _configure(self):
         super(Actor, self)._configure()
         self.id             = self.inventory.id
+        self.type           = self.inventory.type
         self.sname          = self.inventory.sname
         self.server         = self.inventory.server
         self.description    = self.inventory.description
