909bbe8d6903494e00c6604a8d4fa2120d833fd2
Alexander Dementsov <dexity@gmail.com>
2010-09-12 05:00:52 +0000

    Vulcan is now consists of just two components. With inidb.py script I created record in db for Vulcan and edited VULCAN.py in dom/... directory.
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@3104 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/dom/neutron_experiment_simulations/instruments/VULCAN.py
+++ vnfb/dom/neutron_experiment_simulations/instruments/VULCAN.py
@@ -11,17 +11,38 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+components = [
+    ('source',
+     ('MonochromaticSource', {'energy': 47.043}),
+     ( (0,0,0), (0,0,0), '' )
+     ),
+    ('monitor',
+     ('TofMonitor', {'tmin':0, 'tmax': 2e-3}),
+     ( (0,0,3), (0,0,0), '' )
+     ),
+    ]
 
-def createInstrument(director):
-    return cinstr(
-        director,
-        name='VULCAN',
-        short_description='VULCAN. place holder',
-        long_description='long description here',
-        category='engineering diffraction',
-        creator='vnf', date='08/11/2008',
-        components=[],
-        )
+instrument = {
+    'name': 'VULCAN',
+    'short_description': 'SNS diffractometer',
+    'long_description': 'VULCAN is a diffractometer at the Spallation Neutron Source intended for measurements of deformation, residual stress related studies, spatial mapping of chemistry, microstructure, and texture.',
+    'category': 'engineering diffraction',
+    'creator': 'vnf',
+    'date': '9/11/2010',
+    'components': components,
+    }
+
+
+#def createInstrument(director):
+#    return cinstr(
+#        director,
+#        name='VULCAN',
+#        short_description='VULCAN. place holder',
+#        long_description='long description here',
+#        category='engineering diffraction',
+#        creator='vnf', date='08/11/2008',
+#        components=[],
+#        )
 
 
 from _utils import ccomp, cinstr
