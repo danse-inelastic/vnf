30722439db72bc5ab4db828c927c05c6ed2c5c38
Jiao Lin <linjiao@caltech.edu>
2010-09-19 21:25:34 +0000

    SQE kernel now works
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@3143 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ vnfb/tests/content/components/job_builders/neutronexperiment/kernels/SQEKernel_TestCase.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                      (C) 2006-2010  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+standalone = True
+
+
+from TestCaseBase import createTestCase, TestAppBase
+
+class TestApp(TestAppBase):
+    
+    
+    from vnfb.dom.scattering_kernels.ins.SQEKernel import SQEKernel as Kernel
+    
+    
+    kernelid = 'testjobbuilder'
+    def getKernel(self):
+        orm = self.clerk.orm
+        db = orm.db
+        KernelTable = orm(self.Kernel)
+        ks = db.query(KernelTable).filter_by(id=self.kernelid).all()
+        if not ks:
+            return self.createKernel()
+        return orm.record2object(ks[0])
+    
+    
+    def createKernel(self):
+        orm = self.clerk.orm
+        from vnfb.dom.material_simulations.SQE import SQE
+        sqe = orm.load(SQE, id='histogram-example')
+        
+        k = self.Kernel()
+        # r.id = self.kernelid
+        k.sqe = sqe
+        k.Emin = -49.
+        k.Emax = 49.
+        k.Qmin = 0
+        k.Qmax = 12.
+        orm.save(k, id=self.kernelid)
+        return k
+
+
+TestCase = createTestCase(TestApp)
+
+
+def main():
+    import unittest
+    unittest.main()
+    return
+
+
+if __name__ == '__main__': main()
+
+
+# version
+__id__ = "$Id$"
+
+# End of file 
