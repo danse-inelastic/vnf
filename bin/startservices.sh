ab3285531bf1e171bce7924d78d78f0779ac7fe4
Jiao Lin <linjiao@caltech.edu>
2010-10-11 20:45:12 +0000

    start(stop)servers.sh: now start/stop monitors as well
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@3179 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- bin/startservices.sh
+++ bin/startservices.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
 ipad.py --config=/tmp/luban-services,../config --- --home=../config
+spawn-daemon.py  -cmd='./timer.py --command="./runmonitors.sh `pwd`" -interval=10*minute' -home=. 
