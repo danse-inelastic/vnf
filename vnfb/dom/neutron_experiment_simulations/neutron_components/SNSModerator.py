6a0ef2aeb5d30fea23fe86bb00607e5cc6611aa5
Jiao Lin <linjiao@caltech.edu>
2010-11-11 00:57:06 +0000

    vnfb/dom/.../neutron_components/SNSModerator.py: added tips for properties
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@3238 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/dom/neutron_experiment_simulations/neutron_components/SNSModerator.py
+++ vnfb/dom/neutron_experiment_simulations/neutron_components/SNSModerator.py
@@ -43,14 +43,29 @@ InvBase=base.Inventory
 class Inventory(InvBase):
     
     width = InvBase.d.float(name='width', default=0.1)
+    width.tip = "width of the moderator. unit: meter"
+
     height = InvBase.d.float(name='height', default=0.1)
+    height.tip = "height of the moderator. unit: meter"
+
     dist = InvBase.d.float(name='dist', default=2.5)
+    dist.tip = "distance from moderator to its target. unit: meter"
+
     xw = InvBase.d.float(name='xw', default=0.1)
+    xw.tip = "x (horizontal) dimension of the target. unit: meter"
+
     yh = InvBase.d.float(name='yh', default=0.1)
+    yh.tip = "y (vertical) dimension of the target. unit: meter"
+    
     Emin = InvBase.d.float(name='Emin', default=0)
+    Emin.tip = 'minimum value for energy of neutrons. unit: meV'
+
     Emax = InvBase.d.float(name='Emax', default=100)
+    Emax.tip = 'maximum value for energy of neutrons. unit: meV'
+
     neutronprofile = InvBase.d.reference(
         name='neutronprofile', targettype=SNSModeratorMCSimulatedData, owned=False)
+    neutronprofile.tip = 'Select a neutron profile for the instrument in which you want to run your experiment'
 
     dbtablename = 'snsmoderators'
 
