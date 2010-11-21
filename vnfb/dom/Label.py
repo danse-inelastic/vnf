c011c9de664ff7f5578a6497b3e28cda9c5b1f25
Jiao Lin <linjiao@caltech.edu>
2010-11-21 19:32:26 +0000

    added a special label "private" so users could mark records in atomic structure table view as private. todo: 1. remove private records from table view for other users 2. implement this on all table views
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@3268 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/dom/Label.py
+++ vnfb/dom/Label.py
@@ -11,6 +11,24 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+'''
+This table gives users a way to label his data objects (records).
+
+fields:
+ creator: 
+ date:
+ labelname: name of the label
+ entity: the entity (db record) this label is about
+ targettable: some labels are specific to a table (view). this
+    property gives the name of the table (view).
+
+Note
+* When entity is empty, a label record represents a label without 
+  target. This is useful for getting all the names of labels, for
+  example.
+* There are labels for common purpose. See common_labels.
+'''
+
 
 from OwnedObject import OwnedObject as base
 class Label(base):
@@ -22,6 +40,9 @@ class Label(base):
     labelname = dsaw.db.varchar(name='labelname', length=64)
     entity = dsaw.db.versatileReference(name = 'entity')
     targettable = dsaw.db.varchar(name='targettable', length=64)
+
+
+common_labels = ['private']
 
 
 # version
