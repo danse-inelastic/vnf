2a2a52577fee773e692161c3642095a64bb12abf
Jiao Lin <linjiao@caltech.edu>
2010-11-22 18:51:43 +0000

    'private' label now works for "simulations" table view
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@3271 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/utils/query/labels.py
+++ vnfb/vnfb/utils/query/labels.py
@@ -13,7 +13,7 @@
 
 
 from _ import *
-from vnfb.dom.Label import Label
+from vnfb.dom.Label import Label, common_labels
 
 
 def select_labeled_entities_clause(name, type, db):
@@ -24,12 +24,18 @@ def select_labeled_entities_clause(name, type, db):
       type: type of the entity
     '''
     sL = db._tablemap.TableToSATable(Label)
+    if name in common_labels:
+        whereclause="labelname='%s'" % (name,)
+    else:
+        whereclause="labelname='%s' and targettable='%s'" % (
+            name, type)
+        
     labelq = sqlalchemy.select(
         [sL.c.entity.label('entity'),
          sL.c.labelname.label('label'),
          ],
-        whereclause="labelname='%s' and targettable='%s'" % (name, type))\
-        .alias('labelq')
+        whereclause=whereclause,
+        ) .alias('labelq')
 
     sGP = db._tablemap.TableToSATable(global_pointer)
 
