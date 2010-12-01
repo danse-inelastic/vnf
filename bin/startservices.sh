a43c81e11102a2055db0c73e1e564bddb64d883f
Jiao Lin <linjiao@caltech.edu>
2010-12-01 04:36:09 +0000

    vnfb/bin/startservices.sh: minor (a bit of documentation)
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@3301 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- bin/startservices.sh
+++ bin/startservices.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env sh
+
+# script to start various vnf services
+# should be run as a vnf system administrator
+# requirements of vnf sys admin:
+#  1. has a ssh key accepted by computing servers 
+#  2. has set up a ssh agent and the ssh key in (1) is added
+# 
+
 ./run-daemons.sh
 ./cron-daemons.sh
 
