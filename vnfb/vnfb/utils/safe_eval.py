9b68c3709bd024a7ab31e81d88aee649b0ec20a2
Jiao Lin <linjiao@caltech.edu>
2010-12-14 21:37:40 +0000

    vnfb.utils: safe_eval now a simple import from luban.utils
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@3340 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/utils/safe_eval.py
+++ vnfb/vnfb/utils/safe_eval.py
@@ -1,13 +1,1 @@
-import sys
-
-vinfo = sys.version_info
-
-if vinfo[0] == 2:
-    if vinfo[1] == 5:
-        from safe_eval_25 import *
-    elif vinfo[1] == 6:
-        from safe_eval_26 import *
-    else:
-        raise NotImplementedError
-else:
-    raise NotImplementedError
+from luban.utils.safe_eval import safe_eval
