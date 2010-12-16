ac2091157caa7bd03306f0021ec2e7c729fd0a88
Jiao Lin <linjiao@caltech.edu>
2010-12-16 00:22:06 +0000

    vnf 1.0: applied r3340, r3341, r3342
    git-svn-id: svn://danse.us/VNET/vnf/releases/1.0@3352 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/content/__init__.py
+++ vnfb/vnfb/content/__init__.py
@@ -16,6 +16,24 @@ def announcement():
     return Announcement()
 
 
+def safe_eval_action(s):
+    """evaluate an luban action
+
+    e.g. safe_eval_action('load(actor="login")')
+    """
+    s = 'action=%s' % s
+    from luban.content import load, alert, select
+    c = {
+        'action': None, 
+        'load': load,
+        'alert': alert,
+        'select': select,
+        }
+    from vnfb.utils.safe_eval import safe_eval
+    safe_eval(s, c)
+    return c['action']
+
+
 # version
 __id__ = "$Id$"
 
