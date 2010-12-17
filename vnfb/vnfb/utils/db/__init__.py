9b8936792f42439cdd7fa2a6e849c066dd9e0b87
Jiao Lin <linjiao@caltech.edu>
2010-12-17 18:35:53 +0000

    vnf 1.0: applied r3359-r3361
    git-svn-id: svn://danse.us/VNET/vnf/releases/1.0@3362 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/utils/db/__init__.py
+++ vnfb/vnfb/utils/db/__init__.py
@@ -11,6 +11,22 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+
+def destroy(record, domaccess, username):
+    if hasattr(record, 'creator') and record.creator != username:
+        raise RuntimeError, "Cannot destory because you are not owner. You: %s, record owner: %s" % (username, record.creator)
+    
+    from findreferrals import hasreferral
+    if hasreferral(record, domaccess):
+        raise RuntimeError, "%s:%s still in use" % (
+            record.__class__.__name__, record.id)
+
+    orm = domaccess.orm
+    obj = orm.record2object(record)
+    orm.destroy(obj)
+    return
+
+
 # version
 __id__ = "$Id$"
 
