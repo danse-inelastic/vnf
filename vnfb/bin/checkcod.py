d24cfe44f71922f5972d40ab3674686e2158e03b
J Brandon Keith <jbrkeith@gmail.com>
2011-01-04 18:07:09 +0000

    * migrating all checkcod and cod multiple structure import additions to vnf1.0
    git-svn-id: svn://danse.us/VNET/vnf/releases/1.0@3424 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ vnfb/bin/checkcod.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                Jiao Lin
+#                      California Institute of Technology
+#                      (C) 2006-2010  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+from luban.applications.UIApp import UIApp as base
+
+
+class App(base):
+
+
+    class Inventory(base.Inventory):
+
+        import pyre.inventory
+
+        host = pyre.inventory.str('host')
+        user = pyre.inventory.str('user')
+        passwd = pyre.inventory.str('passwd')
+
+
+    def main(self, *args, **kwds):
+        host = self.inventory.host
+        user = self.inventory.user
+        passwd = self.inventory.passwd
+        
+        import MySQLdb
+        try:
+            conn = MySQLdb.connect(
+                host = host,
+                user = user,
+                passwd = passwd,
+                db = 'cod',
+                )
+        except:
+            self._sendAlert('cod', sys.exc_info()[0])
+        return
+
+    
+    def _sendAlert(self, server, error):
+        from vnfb.utils.communications import announce
+        announce(self, 'cod-server-down', server, error)
+        return
+    
+    
+    def __init__(self):
+        base.__init__(self, 'checkcod')
+        return
+
+
+import sys
+
+
+def main():
+    import journal
+    journal.debug('checkcod').activate()
+    app = App()
+    return app.run()
+
+
+# main
+if __name__ == '__main__':
+    # invoke the application shell
+    main()
+
+
+# version
+__id__ = "$Id: checkcod.py 3385 2010-12-23 00:05:31Z linjiao $"
+
+# End of file 
---
+++ vnfb/config/checkcod.pml
@@ -0,0 +1,9 @@
+<!DOCTYPE inventory>
+
+<inventory>
+  <component name='checkcod'>
+    <property name='host'>vnf-dev.caltech.edu</property>
+    <property name='user'>vnf</property>
+    <property name='passwd'>vnfvnf</property>
+  </component>
+</inventory>
