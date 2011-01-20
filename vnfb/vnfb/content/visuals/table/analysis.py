ae8b409929bbf8b8d3b3914ff28fecb8ebc61e01
J Brandon Keith <jbrkeith@gmail.com>
2011-01-20 02:24:00 +0000

    * adding the latest analysis changes back to 1.0 version
    git-svn-id: svn://danse.us/VNET/vnf/releases/1.0@3473 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/content/visuals/table/analysis.py
+++ vnfb/vnfb/content/visuals/table/analysis.py
@@ -22,6 +22,7 @@ class model(Model):
     #        visualize = Model.descriptors.link(name='visualize')
     #        chemical_formula = Model.descriptors.str(name='chemical_formula')
     #        simulation = Model.descriptors.str(name='simulation')
+    creator = Model.descriptors.str(name='creator')
     created = Model.descriptors.date(name='created')
 
     row_identifiers = ['id']
@@ -33,6 +34,7 @@ columns = [
     #        View.Column(label='Visualize', measure='visualize'),
     #        View.Column(label='Chemical_formula', measure='chemical_formula'),
     #        View.Column(label='Simulation', measure='simulation'),
+    View.Column(label='Creator', measure='creator'),
     View.Column(label='Date created', measure='created'),
     ]
 
@@ -42,7 +44,6 @@ def view(cols, editable=True):
     columns = filter(lambda col: col.measure in cols, columns)
     view = View(columns=columns, editable=editable)
     return view
-
 
 def getSelected(record): return False
 def getId(record):
@@ -71,27 +72,23 @@ def getDescription(record):
 ## def getSimulation(record):
 ##     return 'not implemented'
 ##     #return record.chemical_formula
+def getCreator(record):
+    return record.creator
 def getCreated(record):
     date = record.date
     return str(date)
 
-                
 def table(analyses, cols, director, editable=True):
     global view
     view = view(cols, editable=editable)
-    
-    import operator
     value_generators = [
         eval('get'+col.measure.capitalize())
         for col in view.columns]
     record2tuple = lambda record: [g(record) for g in value_generators]
     data = map(record2tuple, analyses)
-
     table = Table(model=model, data=data, 
                   view=view, id='analysis-table')
-
     return table
-
 
 # version
 __id__ = "$Id$"
