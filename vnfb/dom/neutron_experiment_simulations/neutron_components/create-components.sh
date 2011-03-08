3a0ac35ad78bbfefc5b30426ceb659354d282cb7
Alexander Dementsov <dexity@gmail.com>
2011-03-08 20:05:15 +0000

    Created stub for VulcanDetectorSystem
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@3555 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/dom/neutron_experiment_simulations/neutron_components/create-components.sh
+++ vnfb/dom/neutron_experiment_simulations/neutron_components/create-components.sh
@@ -6,7 +6,7 @@
 #./mcstas-component-to-dom.py  -type=TOF_monitor2 -category=monitors --base=Monitor --tablebase=MonitorTableBase --newname=TofMonitor --skip-props=filename
 #./mcstas-component-to-dom.py  -type=Q_monitor -category=monitors --base=Monitor --tablebase=MonitorTableBase --newname=QMonitor --skip-props=filename
 #./mcstas-component-to-dom.py  -type=PSD_monitor_4PI -category=monitors --base=Monitor --tablebase=MonitorTableBase --newname=SphericalPSD --skip-props=filename
-
+#./mcstas-component-to-dom.py  -type=VulcanDetectorSystem -category=monitors --base=Monitor --tablebase=MonitorTableBase --newname=VulcanDetectorSystem --skip-props=filename
 
 ./mcstas-component-to-dom.py  -type=Channeled_guide -category=obsolete --newname=ChanneledGuide
 ./mcstas-component-to-dom.py  -type=Fermi_chop2 -category=optics --newname=FermiChopper --skip-props=max_iter
