667c367fb3625c39bebd410cea3f9a99d1fb947b
Alexander Dementsov <dexity@gmail.com>
2011-03-08 23:46:57 +0000

    Created interface for vulcan detector system
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@3559 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/dom/neutron_experiment_simulations/neutron_components/VulcanDetectorSystem.py
+++ vnfb/vnfb/dom/neutron_experiment_simulations/neutron_components/VulcanDetectorSystem.py
@@ -14,32 +14,69 @@
 from MonitorBase import MonitorBase as base
 class VulcanDetectorSystem(base):
 
-    abstract = False
+    abstract    = False
+    
+    tmin        = 0
+    tmax        = 0.1
+    nt          = 100
+    wmin        = 0
+    wmax        = 10
+    nw          = 100
+    xwidth      = 0.770
+    yheight     = 0.385
 
     def customizeLubanObjectDrawer(self, drawer):
-        drawer.sequence = [
-            'properties',
-            #'hierarchy',
-            ]
         drawer.mold.sequence = [
             'componentname', 'short_description',
             'referencename', 'position', 'orientation',
+            'tmin', 'tmax', 'nt',
+            'wmin', 'wmax', 'nw',
+            'xwidth', 'yheight'
             ]
 
 InvBase=base.Inventory
 class Inventory(InvBase):
 
-#    tofmin = InvBase.d.float(name='tofmin', default=3000., validator=InvBase.v.positive)
-#    tofmin.tip = 'minimum tof. unit: microsecond'
-    pass
+    tmin        = InvBase.d.float(name='tmin', default=0., validator=InvBase.v.positive)
+    tmin.tip    = 'Minimum TOF, seconds'
+    tmin.help   = tmin.tip
+
+    tmax        = InvBase.d.float(name='tmax', default=0.1, validator=InvBase.v.positive)
+    tmax.tip    = 'Maximum TOF, seconds'
+    tmax.help   = tmax.tip
+
+    nt          = InvBase.d.int(name='nt', default=100, validator=InvBase.v.positive)
+    nt.tip      = 'Number of divisions in time axes'
+    nt.help     = nt.tip
+
+    wmin        = InvBase.d.float(name='wmin', default=0., validator=InvBase.v.positive)
+    wmin.tip    = 'Minimum of wavelength, AA'
+    wmin.help   = wmin.tip
+
+    wmax        = InvBase.d.float(name='wmax', default=10., validator=InvBase.v.positive)
+    wmax.tip    = 'Maximum of wavelength, AA'
+    wmax.help   = wmax.tip
+    
+    nw          = InvBase.d.int(name='nw', default=100, validator=InvBase.v.positive)
+    nw.tip      = 'Number of divisions in wavelength axes'
+    nw.help     = nw.tip
+    
+    xwidth      = InvBase.d.float(name='xwidth', default=0.770, validator=InvBase.v.positive)
+    xwidth.tip  = 'Width of monitors, m'
+    xwidth.help = xwidth.tip
+
+    yheight     = InvBase.d.float(name='yheight', default=0.385, validator=InvBase.v.positive)
+    yheight.tip = 'Height of monitors, m'
+    yheight.help = yheight.tip
+
+    dbtablename = 'vulcan_detector_system'
 
 VulcanDetectorSystem.Inventory = Inventory
 del Inventory
 
 
-from _ import MonitorTableBase
-VulcanDetectorSystemTable = o2t(
-    VulcanDetectorSystem, {'subclassFrom': MonitorTableBase})
+from _ import o2t, MonitorTableBase
+VulcanDetectorSystemTable = o2t(VulcanDetectorSystem, {'subclassFrom': MonitorTableBase})
 
 __date__ = "$Mar 8, 2011 11:57:08 AM$"
 
