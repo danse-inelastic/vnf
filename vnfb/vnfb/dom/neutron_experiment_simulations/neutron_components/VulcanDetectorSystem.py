3f97f5c16224ccd5ff55ff941a0fba69b331a8c3
Alexander Dementsov <dexity@gmail.com>
2011-03-08 21:55:49 +0000

    Minor updates
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@3557 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/vnfb/dom/neutron_experiment_simulations/neutron_components/VulcanDetectorSystem.py
+++ vnfb/vnfb/dom/neutron_experiment_simulations/neutron_components/VulcanDetectorSystem.py
@@ -16,6 +16,30 @@ class VulcanDetectorSystem(base):
 
     abstract = False
 
+    def customizeLubanObjectDrawer(self, drawer):
+        drawer.sequence = [
+            'properties',
+            #'hierarchy',
+            ]
+        drawer.mold.sequence = [
+            'componentname', 'short_description',
+            'referencename', 'position', 'orientation',
+            ]
+
+InvBase=base.Inventory
+class Inventory(InvBase):
+
+#    tofmin = InvBase.d.float(name='tofmin', default=3000., validator=InvBase.v.positive)
+#    tofmin.tip = 'minimum tof. unit: microsecond'
+    pass
+
+VulcanDetectorSystem.Inventory = Inventory
+del Inventory
+
+
+from _ import MonitorTableBase
+VulcanDetectorSystemTable = o2t(
+    VulcanDetectorSystem, {'subclassFrom': MonitorTableBase})
 
 __date__ = "$Mar 8, 2011 11:57:08 AM$"
 
