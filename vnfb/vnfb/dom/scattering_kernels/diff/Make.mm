fbd71903fc43c031f6d5fb20bc7f8c23644f618d
Alexander Dementsov <dexity@gmail.com>
2011-03-09 05:31:48 +0000

    Added stub files for powder diffraction kernel
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban@3564 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ vnfb/vnfb/dom/scattering_kernels/diff/Make.mm
@@ -0,0 +1,48 @@
+# -*- Makefile -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Michael A.G. Aivazis
+#                        California Institute of Technology
+#                        (C) 1998-2004  All Rights Reserved
+#
+# <LicenseText>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+PROJECT = vnfb
+PACKAGE = dom/scattering_kernels/diff
+
+
+# directory structure
+
+BUILD_DIRS = \
+
+OTHER_DIRS = \
+
+RECURSE_DIRS = $(BUILD_DIRS) $(OTHER_DIRS)
+
+
+#--------------------------------------------------------------------------
+#
+
+all: export
+	BLD_ACTION="all" $(MM) recurse
+
+
+#--------------------------------------------------------------------------
+#
+# export
+
+EXPORT_PYTHON_MODULES = \
+	PowderDiffractionKernel.py \
+	_.py \
+	__init__.py \
+
+
+#include doxygen/default.def
+
+export:: export-package-python-modules
+
+
+docs: export-doxygen-docs
