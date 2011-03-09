21253c0fe09f1dd85e03e52841aeaea06132d89c
Alexander Dementsov <dexity@gmail.com>
2011-03-09 20:36:22 +0000

    Plots from DetectorSystem are now displayed
    git-svn-id: svn://danse.us/VNET/vnf/branches/beta-useluban/vnfb@3567 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnfb/dom/scattering_kernels/diff/PowderDiffractionKernel.py
+++ vnfb/dom/scattering_kernels/diff/PowderDiffractionKernel.py
@@ -11,6 +11,39 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+#XXX: Finish?
+
+from AbstractScatteringKernel import AbstractScatteringKernel as base, TableBase
+
+class PowderDiffractionKernel(base):
+
+    # scattering_length = 1.0
+
+    def customizeLubanObjectDrawer(self, drawer):
+        drawer.sequence = ['properties']
+        # drawer.mold.sequence = ['scattering_length']
+        return
+
+    pass
+
+
+
+InvBase = base.Inventory
+class Inventory(InvBase):
+
+    # scattering_length = InvBase.d.float(name = 'scattering_length', default = 1.)
+
+    dbtablename = 'powderdiffractionkernels'
+
+    pass
+
+
+PowderDiffractionKernel.Inventory = Inventory
+del Inventory
+
+
+from _ import o2t
+PowderDiffractionKernelTable = o2t(PowderDiffractionKernel, {'subclassFrom': TableBase},)
 
 __date__ = "$Mar 8, 2011 9:27:32 PM$"
 
