9e6d32f00afff8c951e2d59f9e7ccd11e666fdb3
Jiao Lin <linjiao@caltech.edu>
2011-04-18 02:25:42 +0000

    content/components/dom-access/material_simulations/phonons.odb:  trying a tentative optimization. added a test case.
    git-svn-id: svn://danse.us/VNET/vnf/trunk@3706 056da4b6-1853-4d91-9082-68a8d0adb2e4

---
+++ tests/content/components/actors/orm/phonons_TestCase.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                      (C) 2006-2010  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+'''
+Test of component actors/orm/phonons
+
+Test assumes that
+ 1. there is a phonons record in the db with data
+
+'''
+
+# phonons id.
+phononsid = "bvk-fccAgAt293-N20-df0.2"
+phononsid = "8VDEZDF"
+
+
+# application
+from vnf.testing.TestAppBase import Application as base
+class TestApp(base):
+
+
+    def main(self, testFacility, *args, **kwds):
+        self.actor = self.retrieveActor('orm/phonons')
+        self.actor.inventory.id = phononsid
+        
+        testFacility.assert_(self.actor is not None)
+        self.testFacility = testFacility
+
+        self.chroot()
+        self.test1()
+        self.test2()
+        return 
+
+
+    def test1(self):
+        actor = self.actor
+        phonons = actor._load(self)
+        print actor._createPlotAndDataLink(phonons, self)
+        return
+
+
+    def test2(self):
+        actor = self.actor
+        actor.createGraphicalView(self)
+        return
+
+
+
+import unittest
+class TestCase(unittest.TestCase):
+
+    def test1(self):
+        app = TestApp('main')
+        app.run(self)
+        return
+
+def main():
+    unittest.main()
+    return
+
+if __name__ == '__main__': main()
+
+
+# version
+__id__ = "$Id$"
+
+# End of file 
