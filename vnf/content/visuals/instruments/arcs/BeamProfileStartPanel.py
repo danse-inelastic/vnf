79f5390764094006851ca5c56181bc4e7eec57e0
Jiao Lin <linjiao@caltech.edu>
2011-05-02 17:43:49 +0000

    arcs-beam-profile app: refactored more so we can reuse the structure
    git-svn-id: svn://danse.us/VNET/vnf/trunk@3775 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnf/content/visuals/instruments/arcs/BeamProfileStartPanel.py
+++ vnf/content/visuals/instruments/arcs/BeamProfileStartPanel.py
@@ -17,26 +17,13 @@ from StartPanelFactoryBase import Factory as base
 
 class Factory(base):
 
-    def build(self):
-        director = self.director
+    name = 'arcs-beam-profile'
+    title = 'Beam profile'
+    
 
-        # container
-        container = luban.content.document()
-
-        # where am I?
-        whereami = self.buildViewIndicator()
-        container.add(whereami)
-
-        # the main visual
-        visual = luban.content.document(
-            id='arcs-beam-profile-panel', title="Beam profile")
-        visual.Class = 'app-container'
-        container.add(visual)
-
-        # toolbar
+    def buildToolbar(self):
         toolbar = luban.content.toolbar(
             id='arcs-beam-toolbar', Class='app-toolbar')
-        visual.add(toolbar)
 
         # reload button in toolbar
         reload = luban.content.load(
@@ -48,51 +35,29 @@ class Factory(base):
         loadprofilestable = luban.content.load(
             actor='instruments/arcs/beam-profile',
             routine='showProfilesTable')
-        b = luban.content.button(label='Existing profiles', onclick=loadprofilestable)
+        b = luban.content.button(
+            label='Existing profiles',
+            onclick=loadprofilestable)
         toolbar.add(b)
+        return toolbar
 
-        doc = luban.content.document(id='arcs-beam-work-panel'); visual.add(doc)
-        grid = luban.content.grid(id='arcs-beam-compute-profile-panel'); doc.add(grid)
-        # grid.addClass('align-top')
-        row = grid.row()
-        # cell for user input
-        inputcell = row.cell(Class='align-top')
-        # cell for id holder
-        idholdercell = row.cell()
-        # cell for update button
-        updatecell = row.cell(Class='align-top update-button-cell')
-        # cell for plots
-        plotscell = row.cell(Class='align-top')
-        plotscell.addClass('app-output-cell')
 
-        # input form
-        inputcell.oncreate = luban.content.select(element=inputcell).append(
-            luban.content.load(actor='orm/arcsbeamconfigurations', routine='edit')
+    def buildInputCellOnCreateAction(self, inputcell):
+        return luban.content.select(element=inputcell).append(
+            luban.content.load(
+                actor='orm/arcsbeamconfigurations', 
+                routine='edit')
             )
-
-        #
-        idholder = luban.content.paragraph(id='idholder', hidden=True)
-        idholdercell.add(idholder)
-
-        # update button
-        update = luban.content.load(
+        
+    
+    def buildUpdateButtonOnClickAction(self, inputcell, idholder):
+        return luban.content.load(
             actor='instruments/arcs/beam-profile', 
             routine='update',
             id = luban.content.select(element=idholder).getAttr('text'),
-            formids = luban.content.select(element=inputcell).findDescendentIDs(type='form'),
+            formids = luban.content.select(element=inputcell)\
+                .findDescendentIDs(type='form'),
             )
-        b = luban.content.button(label='===>>', onclick=update)
-        b.tip = 'Run simulation or get simulation results'
-        b.Class = 'run-button'
-        updatecell.add(b)
-
-        # output region
-        doc = luban.content.document(id='output', title='Outputs')
-        plotscell.add(doc)
-        # doc.paragraph(text='Plots go here')
-        doc.document(id='main-display-area')
-
-        return container
 
 
 # version
--- vnf/content/visuals/instruments/arcs/StartPanelFactoryBase.py
+++ vnf/content/visuals/instruments/arcs/StartPanelFactoryBase.py
@@ -16,13 +16,84 @@ import luban.content
 
 class Factory(object):
 
+    name = None # name of this factory. eg: "arcs-beam-profile"
+    title = None # title of the visual. eg: "Beam profile"
+
     def __init__(self, director):
         self.director = director
         return
-    
+
+
+    def buildToolbar(self):
+        raise NotImplementedError
+
+
+    def buildInputCellOnCreateAction(self, inputcell):
+        raise NotImplementedError
+
+
+    def buildUpdateButtonOnClickAction(self, inputcell, idholder):
+        raise NotImplementedError
+
 
     def build(self):
-        raise NotImplementedError
+        director = self.director
+
+        # container
+        container = luban.content.document()
+
+        # where am I?
+        whereami = self.buildViewIndicator()
+        container.add(whereami)
+
+        # the main visual
+        visual = luban.content.document(
+            id='%s-panel' % self.name, title=self.title)
+        visual.Class = 'app-container'
+        container.add(visual)
+
+        # toolbar
+        toolbar = self.buildToolbar()
+        visual.add(toolbar)
+
+        # the input -> run button -> output structure
+        doc = luban.content.document(id='%s-work-panel' % self.name); 
+        visual.add(doc)
+        grid = luban.content.grid(id='%s-compute-profile-panel'% self.name);
+        doc.add(grid)
+        # grid.addClass('align-top')
+        row = grid.row()
+        # cell for user input
+        inputcell = row.cell(Class='align-top')
+        # cell for id holder
+        idholdercell = row.cell()
+        # cell for update button
+        updatecell = row.cell(Class='align-top update-button-cell')
+        # cell for plots
+        outputcell = row.cell(Class='align-top')
+        outputcell.addClass('app-output-cell')
+
+        # input form
+        inputcell.oncreate = self.buildInputCellOnCreateAction(inputcell)
+
+        #
+        idholder = luban.content.paragraph(id='idholder', hidden=True)
+        idholdercell.add(idholder)
+
+        # update button
+        update = self.buildUpdateButtonOnClickAction(inputcell, idholder)
+        b = luban.content.button(label='===>>', onclick=update)
+        b.tip = 'Run simulation or get simulation results'
+        b.Class = 'run-button'
+        updatecell.add(b)
+
+        # output region
+        doc = luban.content.document(id='output', title='Outputs')
+        outputcell.add(doc)
+        # doc.paragraph(text='Plots go here')
+        doc.document(id='main-display-area')
+
+        return container
 
 
     def buildViewIndicator(self):
