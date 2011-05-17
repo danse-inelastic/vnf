bccd1fd934a9a6a5ebd388f41f13c22248002e43
Jiao Lin <linjiao@caltech.edu>
2011-05-17 22:09:00 +0000

    arcs beam app: minor refactor of "results-view" factory
    git-svn-id: svn://danse.us/VNET/vnf/trunk@3788 056da4b6-1853-4d91-9082-68a8d0adb2e4

--- vnf/content/visuals/instruments/arcs/BeamProfileResultsView.py
+++ vnf/content/visuals/instruments/arcs/BeamProfileResultsView.py
@@ -17,80 +17,46 @@ import luban.content
 from ResultsViewFactoryBase import Factory as base
 class Factory(base):
 
-
-    def build(self, id=None, **kwds):
+    def _createOverview(self, tabs, id=None):
         #
         director = self.director
-
-        # 
-        actor = kwds.get('actor') or self.actor
         
         #
-        container = luban.content.document(id='%s-results-container' % self.name)
-        tabs = luban.content.tabs(); container.add(tabs)
-
-        # first tab
-        tab = createOverview(tabs, director=director, id=id)
-
-        # other tabs
-        tabnames = [
-            'Beam monitors',
-            'Resolution at sample position',
-            'Spatial distribution',
-            'Divergence',
+        domaccess = director.retrieveDOMAccessor('computation')
+        #     
+        computation = domaccess.getComputationRecord('arcsbeamconfigurations', id)
+        #
+        dds = director.dds
+        p = dds.abspath(computation, 'out/props.json')
+        #
+        d = eval(open(p).read())
+        #
+        doc = luban.content.rstdoc()
+        doc.text = [
+            'Computed properties',
+            '-------------------',
             ]
-        for tabname in tabnames:
-            tab = tabs.tab(label=tabname)
-            tab.onselect = luban.content.select(element=tab).replaceContent(
-                luban.content.load(
-                    actor=actor,
-                    routine='loadVisual',
-                    name = 'results_view',
-                    method = "createPanel",
-                    panel = tabname,
-                    id = id,
-                    )
-                )
+        for k, v in d.iteritems():
+            doc.text.append('* %s:%s' % (k,v))
             continue
-        return container
+
+        tab = tabs.tab('Overview')
+        tab.add(doc)
+
+        return tab
 
 
-    def _createHistView(self, id=None, histogram_path=None):
-        """create histogram view for a histogram
-        
-        id: id of the computation
-        histogram_path: relative path of the histogram in the computation directory
-        """
-        from .HistogramView import Factory
-        vf = Factory(
-            actor=self.actor, 
-            director=self.director, 
-            name='beam-profile-histogram')
-        return vf.build(id=id, histogram_path=histogram_path)
-
+    _tabnames = [
+        'Beam monitors', 'Resolution at sample position', 
+        'Spatial distribution', 'Divergence',
+        ]
+    def _tabname2histograms(self, name):
+        histids = panels[name]
+        p = lambda id: 'out/%s.h5' % id
+        return map(p, histids)
     
-    def createPanel(self, id=None, panel=None, **kwds):
-        histids = panels[panel]
-        import collections
-        q = collections.deque(histids)
-        g = luban.content.grid()
-        ncols = 1
-        while q:
-            row = g.row()
-            for i in range(ncols):
-                if not q: break
-                hid = q.popleft()
-                histogram_path = 'out/%s.h5' % hid
-                cell = row.cell()
-                histview = self._createHistView(id=id, histogram_path=histogram_path)
-                cell.add(histview)
-                continue
-            continue
-        container = luban.content.document()
-        container.add(g)
-        return container
 
-        
+
 panels = {
     'Beam monitors':
         ('mon1-itof-focused',
@@ -104,31 +70,6 @@ panels = {
         ('ix_divx', 'ix_divy',
          'iy_divx', 'iy_divy',),
     }
-
-
-def createOverview(tabs, director, id):
-    domaccess = director.retrieveDOMAccessor('computation')
-    #     
-    computation = domaccess.getComputationRecord('arcsbeamconfigurations', id)
-    #
-    dds = director.dds
-    p = dds.abspath(computation, 'out/props.json')
-    #
-    d = eval(open(p).read())
-    #
-    doc = luban.content.rstdoc()
-    doc.text = [
-        'Computed properties',
-        '-------------------',
-        ]
-    for k, v in d.iteritems():
-        doc.text.append('* %s:%s' % (k,v))
-        continue
-
-    tab = tabs.tab('Overview')
-    tab.add(doc)
-    
-    return tab
 
 
 # version
--- vnf/content/visuals/instruments/arcs/ResultsViewFactoryBase.py
+++ vnf/content/visuals/instruments/arcs/ResultsViewFactoryBase.py
@@ -18,10 +18,121 @@ base class for visuals that displays results of a computation
 """
 
 
-from ...AbstractFactory import AbstractFactory as base
+import luban.content
 
+
+from ...AbstractFactory import AbstractFactory as base
 class Factory(base):
 
+    def build(self, id=None, **kwds):
+        """ build results view for the given computation
+
+        this default implementation presents a tabs-view
+
+        tab #1: overview
+        other tabs: each shows a bunch of histograms
+
+        requirements:
+        
+        * self._createOverview method
+        * self._tabnames: names of tabs other than the overview
+        
+        """
+        
+        #
+        director = self.director
+
+        # 
+        actor = kwds.get('actor') or self.actor
+        
+        #
+        container = luban.content.document(id='%s-results-container' % self.name)
+        tabs = luban.content.tabs(); container.add(tabs)
+
+        # first tab
+        tab = self._createOverview(tabs, id=id)
+
+        # other tabs
+        tabnames = self._tabnames
+        for tabname in tabnames:
+            tab = tabs.tab(label=tabname)
+            tab.onselect = luban.content.select(element=tab).replaceContent(
+                luban.content.load(
+                    actor=actor,
+                    routine='loadVisual',
+                    name = 'results_view',
+                    method = "createPanel",
+                    panel = tabname,
+                    id = id,
+                    )
+                )
+            continue
+        return container
+
+
+    def createPanel(self, id=None, panel=None, **kwds):
+        """create result view panel (identified by its name) for the given computation 
+
+        id: id of the computation
+        panel: name of the panel
+
+        this default implementation assumes that we have a map "tabname2histograms"
+        that map a panel name to a list of histogram paths. and we will just
+        create a panel containing a bunch of view of those histograms.
+
+        requirements: 
+          self._tabname2histograms: a method to map tab name to a list of histogram paths
+        """
+        histpaths = self._tabname2histograms(panel)
+        import collections
+        q = collections.deque(histpaths)
+        g = luban.content.grid()
+        ncols = 1
+        while q:
+            row = g.row()
+            for i in range(ncols):
+                if not q: break
+                histogram_path = q.popleft()
+                cell = row.cell()
+                histview = self._createHistView(id=id, histogram_path=histogram_path)
+                cell.add(histview)
+                continue
+            continue
+        container = luban.content.document()
+        container.add(g)
+        return container
+
+        
+    def _createHistView(self, id=None, histogram_path=None):
+        """create histogram view for a histogram
+        
+        id: id of the computation
+        histogram_path: relative path of the histogram in the computation directory
+        """
+        from .HistogramView import Factory
+        vf = Factory(
+            actor=self.actor, 
+            director=self.director, 
+            name='%s-histogram' % self.name)
+        return vf.build(id=id, histogram_path=histogram_path)
+
+
+    def _createOverview(self, tabs, id=None):
+        """in the tabs visual, create a tab for overview, and add content
+        to that tab
+
+        inputs:
+        * tabs: tabs visual into which a new tab of overview will be added
+        * id: id of the computation
+        """
+        raise NotImplementedError
+
+
+    def _tabname2histograms(self, name):
+        """map name of a tab to a list of histogram paths
+        """
+        raise NotImplementedError
+    
     pass 
 
 
